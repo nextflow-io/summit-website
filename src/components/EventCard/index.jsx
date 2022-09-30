e03ac77a6d8ed5d94b97a8b10375fb7bc6dd8f7d
Phil Ewels <phil@seqera.io>
2022-10-01 01:09:05 +0200

    Make location and locationUrl dynamic instead of hardcoded

--- src/components/EventCard/index.jsx
+++ src/components/EventCard/index.jsx
@@ -133,8 +133,8 @@ const EventCard = ({ event, hidden, expanded, isExpandable, onExpand, isChild, d
           <>
             <span className="hidden lg:block mx-2">|</span>
             <div>
-              <Link to="https://goo.gl/maps/K3chvdYLa9BfDpaD9" className="typo-body">
-                Torre Glòries, Avinguda Diagonal, 211, 08018 Barcelona, Spain
+              <Link to={location.locationUrl} className="typo-body">
+                {event.location}
               </Link>
               <LocationIcon className="inline-block h-6 w-6 ml-2" />
             </div>
--- src/templates/event.jsx
+++ src/templates/event.jsx
@@ -89,8 +89,8 @@ const EventPage = ({ data }) => {
               <>
                 <span className="hidden lg:block mx-2">|</span>
                 <div>
-                  <Link to="https://goo.gl/maps/K3chvdYLa9BfDpaD9" className="typo-body">
-                    Torre Glòries, Avinguda Diagonal, 211, 08018 Barcelona, Spain
+                  <Link to={event.locationUrl} className="typo-body">
+                    {event.location}
                   </Link>
                   <LocationIcon className="inline-block h-6 w-6 ml-2" />
                 </div>
