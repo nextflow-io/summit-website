73aac2facdfa12204e02d1aca57aa7fe85f44b97
Oleh Pratsko <olehpratsko@gmail.com>
2023-06-19 17:47:20 +0300

    feat: actibate CountDown

--- src/components/CountDown/index.jsx
+++ src/components/CountDown/index.jsx
@@ -1,6 +1,31 @@
 import React from 'react';
 
+import { useLayoutState } from '../../layout/Context';
+
+const BARCELONA_START_DATE = 'October 12, 2023';
+const BOSTON_START_DATE = 'October 14, 2023';
+
+const daysLeft = (date) => {
+  const oneDay = 24 * 60 * 60 * 1000;
+  const nowDate = new Date();
+  return Math.round((date - nowDate) / oneDay);
+};
+
+const weeksLeft = (toDate) => {
+  const days = daysLeft(toDate);
+  return Math.round(days / 7);
+};
+
+const monthLeft = (toDate) => {
+  const days = daysLeft(toDate);
+  return Math.round(days / 30);
+};
+
 const Counter = () => {
+  const { activeEvent } = useLayoutState();
+
+  const startDate = (activeEvent === 'boston') ? new Date(BOSTON_START_DATE) : new Date(BARCELONA_START_DATE);
+
   return (
     <div>
       <p className="typo-intro">
@@ -9,7 +34,7 @@ const Counter = () => {
       <div className="bg-gray-900 flex rounded-md shadow-xl">
         <div className="p-4 text-center">
           <span className="typo-h4 block">
-            4
+            {monthLeft(startDate)}
           </span>
           <span className="typo-intro block">
             months
@@ -17,7 +42,7 @@ const Counter = () => {
         </div>
         <div className="p-4 text-center">
           <span className="typo-h4 block">
-            17
+            {weeksLeft(startDate)}
           </span>
           <span className="typo-intro block">
             weeks
@@ -25,7 +50,7 @@ const Counter = () => {
         </div>
         <div className="p-4 text-center">
           <span className="typo-h4 block">
-            123
+            {daysLeft(startDate)}
           </span>
           <span className="typo-intro block">
             days
