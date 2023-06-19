d36d5c638204256b035837cfe05c326cb20c4e23
Oleh Pratsko <olehpratsko@gmail.com>
2023-06-19 12:39:27 +0300

    feat: update hero animation

--- src/components/HeroAnimation/index.jsx
+++ src/components/HeroAnimation/index.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import CircleVisualSrc from '../../images/visuals/circle.png';
 
 const HeroAnimation = ({ className }) => {
+
   return (
     <div
       style={{
-        backgroundPosition: 'right',
+        backgroundPosition: 'center center',
         backgroundSize: '96px 96px',
-        backgroundRepeat: 'space',
         backgroundImage: `url(${CircleVisualSrc})`
       }}
       className="absolute inset-0"
     >
       <div
-        className="absolute inset-0"
+        className="absolute inset-0 duration-300 group-hover:scale-105"
         style={{
-          background: "radial-gradient(transparent, #290362)",
+          background: "radial-gradient(calc(96px * 2.5) at calc(50% + 96px * 3), transparent 70%, rgba(41,3,98,0.9))",
         }}
       />
     </div>
