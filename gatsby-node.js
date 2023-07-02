11973cddaa6709a9c8d74f0307d09f5c6c45fb72
Phil Ewels <phil.ewels@seqera.io>
2023-07-02 18:17:02 +0200

    Build: Move to /2022/ subdirectory

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 
 exports.createSchemaCustomization = ({ actions }) => {
   const { createTypes } = actions;
@@ -292,4 +293,11 @@ exports.onCreateWebpackConfig = ({ actions, loaders }) => {
       ],
     },
   });
+};
+
+// Move to /2022/
+exports.onPostBuild = function() {
+  fs.renameSync(path.join(__dirname, 'public'), path.join(__dirname, 'public-tmp'));
+  fs.mkdirSync(path.join(__dirname, 'public'));
+  fs.renameSync(path.join(__dirname, 'public-tmp'), path.join(__dirname, 'public', '2022'));
 };
