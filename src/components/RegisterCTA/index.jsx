1b6e3d72a5b9d81f0c8938a88ef4a18937914e31
Jake Broughton <jake@mythical.fish>
2023-07-18 13:24:50 +0200

    Update colors of RegisterCTA

--- src/components/RegisterCTA/index.jsx
+++ src/components/RegisterCTA/index.jsx
@@ -1,36 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import {
-  Button,
-  Reveal,
-  RevealOnScroll,
-} from 'website-components';
+import { Button, Reveal, RevealOnScroll } from "website-components";
 
-import { useLayoutState } from '../../layout/Context';
+import { useLayoutState } from "../../layout/Context";
 
 const RegisterCTA = () => {
   const { activeEvent } = useLayoutState();
 
   return (
-    <RevealOnScroll className="bg-green-300 text-black">
+    <RevealOnScroll className="bg-nextIndigo text-white">
       <Reveal className="container-sm py-16">
         <div className="max-w-[600px] mx-auto text-center">
           <h2 className="typo-display2">
             Step into the future of data-driven science at the Nextflow SUMMIT
             <br />
-            {activeEvent === 'barcelona' && (
-              <>
-                held in Barcelona, October 16-20.
-              </>
+            {activeEvent === "barcelona" && (
+              <>held in Barcelona, October 16-20.</>
             )}
-            {activeEvent === 'boston' && (
-              <>
-                held in Boston, November 28-30.
-              </>
-            )}
+            {activeEvent === "boston" && <>held in Boston, November 28-30.</>}
           </h2>
-          <div className="mt-4">
-            <Button to={(activeEvent === 'boston' ? '/boston/register/' : '/register/')} variant="accent" size="md" arrow>
+          <div className="mt-5">
+            <Button
+              to={activeEvent === "boston" ? "/boston/register/" : "/register/"}
+              variant="primary"
+              size="md"
+              arrow
+            >
               Register
             </Button>
           </div>
