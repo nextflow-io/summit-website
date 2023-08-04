2e711eb6619232f91f4687c4535e706e72eccbc9
Jake Broughton <jake@mythical.fish>
2023-08-04 11:54:30 +0200

    Key fix

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-export const onRenderBody = ({ setPostBodyComponents, setPreBodyComponents }) => {
+export const onRenderBody = ({ setPostBodyComponents }) => {
   setPostBodyComponents([
-    <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/6705631.js" />,
+    <script
+      type="text/javascript"
+      id="hs-script-loader"
+      key="hs-script-loader"
+      async
+      defer
+      src="//js.hs-scripts.com/6705631.js"
+    />,
   ]);
 };
