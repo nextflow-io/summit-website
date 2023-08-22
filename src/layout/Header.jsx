3a7c537ded8876a40e9b6c7fe0177cabe5335b6a
Phil Ewels <phil.ewels@seqera.io>
2023-08-22 14:31:56 +0200

    Remove mobile nav on index

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -66,52 +66,54 @@ const Header = ({ location }) => {
             <img src={Logo} className="h-8 lg:h-10" alt="" />
           </Link>
           {location && (
-            <div className="lg:flex items-center hidden">
-              {navs.map((nav) => (
-                <Link
-                  key={nav.path}
-                  to={resolvePath(nav.path)}
-                  noBorder
-                  className={classnames(
-                    'bg-black bg-opacity-10 font-body py-1 px-4 rounded-sm mr-px font-normal tracking-wide',
-                    {
-                      'text-white': !location.pathname.includes(nav.path),
-                      'text-green-300': location.pathname.includes(nav.path),
-                    }
-                  )}
+            <>
+              <div className="lg:flex items-center hidden">
+                {navs.map((nav) => (
+                  <Link
+                    key={nav.path}
+                    to={resolvePath(nav.path)}
+                    noBorder
+                    className={classnames(
+                      'bg-black bg-opacity-10 font-body py-1 px-4 rounded-sm mr-px font-normal tracking-wide',
+                      {
+                        'text-white': !location.pathname.includes(nav.path),
+                        'text-green-300': location.pathname.includes(nav.path),
+                      }
+                    )}
+                  >
+                    {resolveTitle(nav.title, nav.bostonTitle)}
+                  </Link>
+                ))}
+                <Button
+                  to={resolvePath('/call-for-abstracts/')}
+                  variant="secondary"
+                  size="sm"
+                  className="hover:opacity-80 ml-4"
+                  noShadow
                 >
-                  {resolveTitle(nav.title, nav.bostonTitle)}
-                </Link>
-              ))}
-              <Button
-                to={resolvePath('/call-for-abstracts/')}
-                variant="secondary"
-                size="sm"
-                className="hover:opacity-80 ml-4"
-                noShadow
-              >
-                Call for abstracts
-              </Button>
-              <Button
-                to={resolvePath('/register/')}
-                variant="primary"
-                size="sm"
-                className="hover:opacity-80 ml-4"
-                noShadow
-              >
-                Register
-              </Button>
-            </div>
+                  Call for abstracts
+                </Button>
+                <Button
+                  to={resolvePath('/register/')}
+                  variant="primary"
+                  size="sm"
+                  className="hover:opacity-80 ml-4"
+                  noShadow
+                >
+                  Register
+                </Button>
+              </div>
+              <div className="lg:hidden">
+                <Button
+                  onClick={() => { toggleMenu(); }}
+                  noShadow
+                  className="text-white"
+                >
+                  <MenuIcon />
+                </Button>
+              </div>
+            </>
           )}
-          <div className="lg:hidden">
-            <Button
-              onClick={() => { toggleMenu(); }}
-              noShadow
-              className="text-white"
-            >
-              <MenuIcon />
-            </Button>
-          </div>
         </div>
       </header>
       <div className={classnames(
@@ -140,40 +142,44 @@ const Header = ({ location }) => {
             </Button>
           </div>
           <div className="flex-1 py-16 overflow-y-auto text-center">
-            {navs.map((nav) => (
-              <div className="mt-4 first:mt-0" key={nav.path}>
-                <Link
-                  key={nav.path}
-                  to={resolvePath(nav.path)}
-                  onClick={closeMenu}
-                  noBorder
-                >
-                  {nav.title}
-                </Link>
-              </div>
-            ))}
-            <div className="mt-4">
-              <Button
-                onClick={() => { handleNav('/call-for-abstracts/') }}
-                variant="secondary"
-                size="sm"
-                className="hover:opacity-80 ml-2"
-                noShadow
-              >
-                Call for abstracts
-              </Button>
-            </div>
-            <div className="mt-4">
-              <Button
-                onClick={() => { handleNav('/register/') }}
-                variant="accent"
-                size="sm"
-                className="hover:opacity-80 ml-2"
-                noShadow
-              >
-                Register
-              </Button>
-            </div>
+            {location && (
+              <>
+                {navs.map((nav) => (
+                  <div className="mt-4 first:mt-0" key={nav.path}>
+                    <Link
+                      key={nav.path}
+                      to={resolvePath(nav.path)}
+                      onClick={closeMenu}
+                      noBorder
+                    >
+                      {nav.title}
+                    </Link>
+                  </div>
+                ))}
+                <div className="mt-4">
+                  <Button
+                    onClick={() => { handleNav('/call-for-abstracts/') }}
+                    variant="secondary"
+                    size="sm"
+                    className="hover:opacity-80 ml-2"
+                    noShadow
+                  >
+                    Call for abstracts
+                  </Button>
+                </div>
+                <div className="mt-4">
+                  <Button
+                    onClick={() => { handleNav('/register/') }}
+                    variant="accent"
+                    size="sm"
+                    className="hover:opacity-80 ml-2"
+                    noShadow
+                  >
+                    Register
+                  </Button>
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
