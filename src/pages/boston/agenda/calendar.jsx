f5653ad94ee2bc6d421ab68b39ca59eeb9bdfeb9
Phil Ewels <phil.ewels@seqera.io>
2023-08-29 14:01:31 +0200

    Calendar: Remove header, include 'back' button

--- src/pages/boston/agenda/calendar.jsx
+++ src/pages/boston/agenda/calendar.jsx
@@ -1,33 +1,12 @@
 import React, { useEffect } from 'react';
 
-import { useStaticQuery, graphql } from 'gatsby';
-import { GatsbyImage as Image, getImage } from 'gatsby-plugin-image';
+import { AngleLeftIcon, Link } from 'website-components';
 
 import Seo from '../../../components/Seo';
-import IntroText from '../../../modules/AgendaPage/IntroText';
 import Tabs from '../../../components/Tabs2';
 
 
 const RegisterPage = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      heroImgBoston: file(relativePath: { eq: "photos/agenda-boston.jpg" }) {
-        childImageSharp {
-          gatsbyImageData(placeholder: NONE)
-        }
-      }
-      bluePattern: file(relativePath: { eq: "visuals/speakers-blue-pattern.png" }) {
-        childImageSharp {
-          gatsbyImageData(placeholder: NONE)
-        }
-      }
-      redPattern: file(relativePath: { eq: "visuals/speakers-red-pattern.png" }) {
-        childImageSharp {
-          gatsbyImageData(placeholder: NONE)
-        }
-      }
-    }
-  `);
   useEffect(() => {
     const script = document.createElement('script');
     script.src = 'https://embed.styledcalendar.com/assets/parent-window.js';
@@ -40,25 +19,14 @@ const RegisterPage = () => {
   return (
     <>
       <Seo title="Nextflow Summit 2023 Agenda - Calendaar" />
-      <div className="pt-20 bg-gray-900 text-white">
-        <div className="container-lg">
-          <div className="row lg:flex-nowrap">
-            <div className="col-full lg:col-6">
-              <IntroText eventLocation="boston" />
-            </div>
-            <div className="col-full lg:col-5 lg:ml-1/12">
-              <Image
-                image={getImage(data.heroImgBoston)}
-                alt="Join us in Boston or virtually"
-                className="rounded-sm shadow-xl"
-                imgClassName="rounded-sm"
-              />
-            </div>
-          </div>
+      <div className="container-lg py-16 bg-gray-900 text-white">
+        <div className="inline-flex items-center hover:text-green-300 mb-4">
+            <AngleLeftIcon className="h-6 w-6 inline-block mr-1" />
+            <Link to="/boston/agenda/" className="typo-intro" noBorder>
+              Back
+            </Link>
         </div>
-      </div>
-      <div className="container-lg text-white pt-36 pb-16" id="events">
-        <h2 className="typo-h5 uppercase text-center mb-4">Agenda</h2>
+        <h1 className="typo-h2 text-green-300">Boston: Calendar</h1>
         <div className="row my-8">
           <div className="col-6">
             <p className='typo-body'>
