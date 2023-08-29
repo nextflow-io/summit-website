d6d40910e819b17aa9fa820a1187660e7e164b55
Phil Ewels <phil.ewels@seqera.io>
2023-08-29 13:56:19 +0200

    Add buttons to subscribe to calendar feeds

--- src/pages/barcelona/agenda/calendar.jsx
+++ src/pages/barcelona/agenda/calendar.jsx
@@ -5,6 +5,7 @@ import { GatsbyImage as Image, getImage } from 'gatsby-plugin-image';
 
 import Seo from '../../../components/Seo';
 import IntroText from '../../../modules/AgendaPage/IntroText';
+import Tabs from '../../../components/Tabs2';
 
 
 const RegisterPage = () => {
@@ -58,6 +59,30 @@ const RegisterPage = () => {
       </div>
       <div className="container-lg text-white pt-36 pb-16" id="events">
         <h2 className="typo-h5 uppercase text-center mb-4">Agenda</h2>
+        <div className="row my-8">
+          <div className="col-6">
+            <p className='typo-body'>
+              Subscribe to Hackathon calendar:
+              <span className="ml-4">
+                <Tabs location="-" >
+                  <Tabs.Item to="https://calendar.google.com/calendar/u/1?cid=Y184NDUxMjEyYjhhOWVhM2UyMzU0ZGIwYTU1Y2MyMTFkZDcxZTQyNTNjYjRkYTExMTRjMzIyZGIwNGVmZTJkOGUxQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20">Google Calendar</Tabs.Item>
+                  <Tabs.Item to="https://calendar.google.com/calendar/ical/c_8451212b8a9ea3e2354db0a55cc211dd71e4253cb4da1114c322db04efe2d8e1%40group.calendar.google.com/public/basic.ics">iCal</Tabs.Item>
+                </Tabs>
+              </span>
+            </p>
+          </div>
+          <div className="col-6 text-right">
+            <p className='typo-body'>
+              Subscribe to Summit calendar:
+              <span className="ml-4">
+                <Tabs location="-" >
+                  <Tabs.Item to="https://calendar.google.com/calendar/u/1?cid=Y18yZTFiZmZjMWNhMzgwMzE5ZDc4ZjJhMmY2OTI0NmVmMjk3Yzg5NTYyMjljMWNlODY1M2UxOWNiMTAxYmNjODBjQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20">Google Calendar</Tabs.Item>
+                  <Tabs.Item to="https://calendar.google.com/calendar/ical/c_2e1bffc1ca380319d78f2a2f69246ef297c8956229c1ce8653e19cb101bcc80c%40group.calendar.google.com/public/basic.ics">iCal</Tabs.Item>
+                </Tabs>
+              </span>
+            </p>
+          </div>
+        </div>
         <div className="calendar-container">
           <iframe src="https://embed.styledcalendar.com/#Qss4ZvPVKjMoFYIc82hS" title="Styled Calendar" className="styled-calendar-container w-full border-0 h-[600px]" data-cy="calendar-embed-iframe"></iframe>
         </div>
--- src/pages/boston/agenda/calendar.jsx
+++ src/pages/boston/agenda/calendar.jsx
@@ -5,6 +5,7 @@ import { GatsbyImage as Image, getImage } from 'gatsby-plugin-image';
 
 import Seo from '../../../components/Seo';
 import IntroText from '../../../modules/AgendaPage/IntroText';
+import Tabs from '../../../components/Tabs2';
 
 
 const RegisterPage = () => {
@@ -58,6 +59,30 @@ const RegisterPage = () => {
       </div>
       <div className="container-lg text-white pt-36 pb-16" id="events">
         <h2 className="typo-h5 uppercase text-center mb-4">Agenda</h2>
+        <div className="row my-8">
+          <div className="col-6">
+            <p className='typo-body'>
+              Subscribe to Hackathon calendar:
+              <span className="ml-4">
+                <Tabs location="-" >
+                  <Tabs.Item to="https://calendar.google.com/calendar/u/1?cid=Y180MzVlYWE2MWM2MjRjZGM1MTJmYWZkZjYxZjI5YzhlZDVhOGM4NzM0NWRmMWM1Zjc2MjE1ZjAxYjVjMGFmYzU4QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20">Google Calendar</Tabs.Item>
+                  <Tabs.Item to="https://calendar.google.com/calendar/ical/c_435eaa61c624cdc512fafdf61f29c8ed5a8c87345df1c5f76215f01b5c0afc58%40group.calendar.google.com/public/basic.ics">iCal</Tabs.Item>
+                </Tabs>
+              </span>
+            </p>
+          </div>
+          <div className="col-6 text-right">
+            <p className='typo-body'>
+              Subscribe to Summit calendar:
+              <span className="ml-4">
+                <Tabs location="-" >
+                  <Tabs.Item to="https://calendar.google.com/calendar/u/1?cid=Y184ZjY0NGMyNTUxM2NjNGQ2YTEzNzcyYzdiYWNhODNlOGZhOWNkN2QyYTMyZWJlMzdmMWIyYzA5NTI4ODhjNTFiQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20">Google Calendar</Tabs.Item>
+                  <Tabs.Item to="https://calendar.google.com/calendar/ical/c_8f644c25513cc4d6a13772c7baca83e8fa9cd7d2a32ebe37f1b2c0952888c51b%40group.calendar.google.com/public/basic.ics">iCal</Tabs.Item>
+                </Tabs>
+              </span>
+            </p>
+          </div>
+        </div>
         <div className="calendar-container">
           <iframe src="https://embed.styledcalendar.com/#A5RyQEwveb0zYn69ncqd" title="Styled Calendar" className="styled-calendar-container w-full border-0 h-[600px]" data-cy="calendar-embed-iframe"></iframe>
         </div>
