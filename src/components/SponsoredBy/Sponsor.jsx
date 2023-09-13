913274490e347f2d2d4137fafd9640dc66308b48
Jake Broughton <jake.broughton@seqera.io>
2023-09-13 15:49:35 +0200

    Add Quilt Sponsor

--- src/components/SponsoredBy/Sponsor.jsx
+++ src/components/SponsoredBy/Sponsor.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
 const Sponsor = ({ children }) => (
-  <div className="w:full sm:w-auto p-1 flex-1">
-    <div className="bg-gray-800 h-24 sm:h-40 max-ws px-4 flex items-center justify-center">{children}</div>
-  </div>
+  <div className="bg-gray-800 h-24 lg:h-40 max-ws px-6 flex items-center justify-center">{children}</div>
 );
 
 export default Sponsor;
