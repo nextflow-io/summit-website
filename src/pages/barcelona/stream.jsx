00fe0702ee84ab28552d17efd226940276a03690
Phil Ewels <phil.ewels@seqera.io>
2023-10-06 15:54:39 +0200

    Add links to Slack

--- src/pages/barcelona/stream.jsx
+++ src/pages/barcelona/stream.jsx
@@ -2,7 +2,7 @@ import { useStaticQuery, graphql } from 'gatsby';
 import { GatsbyImage as Image, getImage } from 'gatsby-plugin-image';
 import React from 'react';
 
-import { Button, List, YoutubeRectangleIcon } from 'website-components';
+import { Button, List, SlackIcon, YoutubeRectangleIcon } from 'website-components';
 
 import GatherIcon from '../../components/icons/GatherIcon';
 import Seo from '../../components/Seo';
@@ -42,6 +42,16 @@ const RegisterPage = () => {
           >
             <GatherIcon className="inline-block h-6 w-6 mr-3" />
             Join Gather
+          </Button>
+          <Button
+            to="https://nextflow.slack.com/channels/summit-2023"
+            variant="primary"
+            size="md"
+            arrow
+            className="mx-3 mb-3"
+          >
+            <SlackIcon className="inline-block h-6 w-6 mr-3" />
+            Ask a question on Slack
           </Button>
           <Button
             to="https://youtube.com/playlist?list=PLPZ8WHdZGxmUotnP-tWRVNtuNWpN7xbpL"
@@ -118,22 +128,45 @@ const RegisterPage = () => {
           <div className="row flex-wrap lg:flex-nowrap">
             <div className="col-full lg:col-6 order-1 lg:order-2 mt-4 lg:mt-0">
               <h2 className="typo-h2 mb-4">Watch on YouTube</h2>
-              <p className="typo-body mt-6">
-                <p className="typo-body mb-4">
-                  If you missed the online registration deadline or are having difficulties with Gather, you can also
-                  watch the live-streams of talks directly on YouTube:
-                </p>
-                <Button
-                  to="https://youtube.com/playlist?list=PLPZ8WHdZGxmUotnP-tWRVNtuNWpN7xbpL"
-                  variant="secondary"
-                  size="sm"
-                  arrow
-                  className="mb-5"
-                >
-                  <YoutubeRectangleIcon className="inline-block h-6 w-6 mr-2" />
-                  Watch on YouTube
-                </Button>
+              <p className="typo-body mt-6 mb-4">
+                If you missed the online registration deadline or are having difficulties with Gather, you can also
+                watch the live-streams of talks directly on YouTube.
               </p>
+              <Button
+                to="https://youtube.com/playlist?list=PLPZ8WHdZGxmUotnP-tWRVNtuNWpN7xbpL"
+                variant="secondary"
+                size="sm"
+                arrow
+                className="mb-5"
+              >
+                <YoutubeRectangleIcon className="inline-block h-6 w-6 mr-2" />
+                Watch on YouTube
+              </Button>
+
+              <p className="typo-body mt-6 mb-4">
+                Questions to speakers and discussion about talks will on the Nextflow Slack, in
+                the <code>#summit-2023</code>&nbsp; channel.
+              </p>
+              <Button
+                to="https://www.nextflow.io/slack-invite.html"
+                variant="secondary"
+                size="sm"
+                arrow
+                className="mb-5 mr-5"
+              >
+                <SlackIcon className="inline-block h-6 w-6 mr-3" />
+                Join Slack
+              </Button>
+              <Button
+                to="https://nextflow.slack.com/channels/summit-2023"
+                variant="secondary"
+                size="sm"
+                arrow
+                className="mb-5"
+              >
+                <SlackIcon className="inline-block h-6 w-6 mr-3" />
+                <code>#summit-2023</code>&nbsp; channel
+              </Button>
             </div>
             <div className="col-full lg:col-6 order-2 lg:order-1">
               <div className="relative border border-green-300 rounded-sm overflow-hidden p-1 bg-black">
