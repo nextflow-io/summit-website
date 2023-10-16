bc2c6b6253fc71952d475f01c4d37f2709fc966b
Jake Broughton <jake.broughton@seqera.io>
2023-10-16 10:02:29 +0200

    Did you know

--- src/modules/FoyerPage/Info.jsx
+++ src/modules/FoyerPage/Info.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import data from './data';
+
+import * as styles from './styles.module.css';
+import classNames from 'classnames';
 
 const DidYouKnow = () => {
+  const [currentItem, setCurrentItem] = useState(0);
+  const [fadeOut, setFadeOut] = useState(false);
+  useEffect(() => {
+    const count = data.length;
+    const interval = setInterval(() => {
+      setFadeOut(true);
+      setTimeout(() => {
+        setCurrentItem((i) => (i + 1) % count);
+        setFadeOut(false);
+      }, 500);
+    }, 4000);
+    return () => clearInterval(interval);
+  }, []);
   return (
     <div>
       <div className="text-3xl font-bold text-blue-500 mb-4">Did you know?</div>
-      <div className="text-3xl font-bold text-white mb-4">The first nextflow commit was made in 2023</div>
+      <div className={classNames('text-3xl font-bold text-white mb-4', styles.info, { [styles.fadeout]: fadeOut })}>
+        {data[currentItem]}
+      </div>
     </div>
   );
 };
---
+++ src/modules/FoyerPage/data.js
@@ -0,0 +1,8 @@
+const didYouKnow = [
+  'The first nextflow commit was made in 2013',
+  'The first nextflow commit was made in 2013',
+  'The first nextflow commit was made in 2013',
+  'The first nextflow commit was made in 2013',
+];
+
+export default didYouKnow;
