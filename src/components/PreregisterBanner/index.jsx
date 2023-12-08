9faa970f5239d565c2b81408eb78027ec58a342f
Jake Broughton <jake.broughton@seqera.io>
2023-12-08 19:54:00 +0100

    Update wording

--- src/components/PreregisterBanner/index.jsx
+++ src/components/PreregisterBanner/index.jsx
@@ -4,7 +4,7 @@ import { Link } from 'gatsby';
 const PreregisterBanner = () => {
   return (
     <Link className="block bg-nextIndigo text-center text-white pb-4 pt-24 md:pt-4 px-6" to="/subscribe-2024/">
-      Want to attend Nextflow Summit 2024? <span className="text-green-300 ml-2">Sign up for updates!</span>
+      Nextflow Summit 2024 dates announced! <span className="text-green-300 ml-2">Sign up for updates!</span>
     </Link>
   );
 };
