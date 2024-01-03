55328f0cc14eeefa523105a3088ff76a9df1ea72
Jake Broughton <jake.broughton@seqera.io>
2024-01-03 17:22:43 +0100

    Add export files

---
+++ src/images/gallery/boston/nf-core-hackathon/index.js
@@ -0,0 +1,83 @@
+import img001 from './001.jpg';
+import img002 from './002.jpg';
+import img003 from './003.jpg';
+import img004 from './004.jpg';
+import img005 from './005.jpg';
+import img006 from './006.jpg';
+import img007 from './007.jpg';
+import img008 from './008.jpg';
+import img009 from './009.jpg';
+import img010 from './010.jpg';
+import img011 from './011.jpg';
+import img012 from './012.jpg';
+import img013 from './013.jpg';
+import img014 from './014.jpg';
+import img015 from './015.jpg';
+import img016 from './016.jpg';
+import img017 from './017.jpg';
+import img018 from './018.jpg';
+import img019 from './019.jpg';
+import img020 from './020.jpg';
+import img021 from './021.jpg';
+import img022 from './022.jpg';
+import img023 from './023.jpg';
+import img024 from './024.jpg';
+import img025 from './025.jpg';
+import img026 from './026.jpg';
+import img027 from './027.jpg';
+import img028 from './028.jpg';
+import img029 from './029.jpg';
+import img030 from './030.jpg';
+import img031 from './031.jpg';
+import img032 from './032.jpg';
+import img033 from './033.jpg';
+import img034 from './034.jpg';
+import img035 from './035.jpg';
+import img036 from './036.jpg';
+import img037 from './037.jpg';
+import img038 from './038.jpg';
+import img039 from './039.jpg';
+import img040 from './040.jpg';
+
+export {
+  img001,
+  img002,
+  img003,
+  img004,
+  img005,
+  img006,
+  img007,
+  img008,
+  img009,
+  img010,
+  img011,
+  img012,
+  img013,
+  img014,
+  img015,
+  img016,
+  img017,
+  img018,
+  img019,
+  img020,
+  img021,
+  img022,
+  img023,
+  img024,
+  img025,
+  img026,
+  img027,
+  img028,
+  img029,
+  img030,
+  img031,
+  img032,
+  img033,
+  img034,
+  img035,
+  img036,
+  img037,
+  img038,
+  img039,
+  img040,
+};
