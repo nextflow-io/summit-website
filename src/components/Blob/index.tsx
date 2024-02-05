90f140ef5260dc7e8e46739ec8d221e1143f2d92
Jake Broughton <jake.broughton@seqera.io>
2024-02-05 16:57:56 +0100

    Simple blob animation

--- src/components/Blob/index.tsx
+++ src/components/Blob/index.tsx
@@ -3,7 +3,12 @@ import React from "react";
 import styles from "./styles.module.css";
 
 const Blob = () => {
-  return <div className={styles.blob} />;
+  return (
+    <>
+      <div className={styles.blob} />
+      <div className={styles.blob2} />
+    </>
+  );
 };
 
 export default Blob;
