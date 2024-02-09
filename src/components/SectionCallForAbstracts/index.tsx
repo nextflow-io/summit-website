5291a9eb8ba64d2e0a40b2febedb52d1202ea1bb
Jake Broughton <jake.broughton@seqera.io>
2024-02-09 14:22:22 +0100

    Page updates

--- src/components/SectionCallForAbstracts/index.tsx
+++ src/components/SectionCallForAbstracts/index.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import Marquee from "../Marquee";
 import call_for_abstracts from "./call_for_abstracts.svg";
 
-type Props = {};
+type Props = {
+  className?: string;
+};
 
-const SectionCallForAbstracts: React.FC<Props> = () => {
+const SectionCallForAbstracts: React.FC<Props> = ({ className }) => {
   return (
-    <Marquee>
+    <Marquee className={className}>
       <img src={call_for_abstracts.src} />
       <img src={call_for_abstracts.src} />
       <img src={call_for_abstracts.src} />
