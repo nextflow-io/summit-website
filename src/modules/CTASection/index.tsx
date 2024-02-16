7a05aef3f488f8155a04c5c2a5eb30bdb4b4c961
Jake Broughton <jake.broughton@seqera.io>
2024-02-16 11:51:27 +0100

    Button updates

--- src/modules/CTASection/index.tsx
+++ src/modules/CTASection/index.tsx
@@ -14,7 +14,7 @@ const CTASection: React.FC<Props> = () => {
           Step into the future of data-driven science at the Nextflow Summit
           held in Boston, this May
         </div>
-        <Button href="/2024/boston/register" arrow wide>
+        <Button href="/2024/boston/register" arrow large>
           Pre-register now
         </Button>
       </div>
