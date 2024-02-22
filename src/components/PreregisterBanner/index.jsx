f1f0d915f06edf535a87a0696f23703612636f22
Jake Broughton <jake.broughton@seqera.io>
2024-02-22 19:41:02 +0100

    Hide banner

--- src/components/PreregisterBanner/index.jsx
+++ src/components/PreregisterBanner/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 const PreregisterBanner = () => {
+  return null;
   return (
     <Link className="block bg-nextIndigo text-center text-white pb-4 pt-24 md:pt-4 px-6" to="/subscribe-2024/">
       Nextflow Summit 2024 dates announced! <span className="text-green-300 ml-2">Sign up for updates!</span>
