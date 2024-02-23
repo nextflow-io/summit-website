aff11e6c9fa259dc0768718f30ab21e2c5842ca4
Jake Broughton <jake.broughton@seqera.io>
2024-02-23 17:25:16 +0100

    Clienside redirect

--- src/pages/subscribe-2024.jsx
+++ src/pages/subscribe-2024.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Seo from '../components/Seo';
 import ContactUs from '../components/ContactUs';
 import Preregister2024 from '../components/Preregister2024';
 
 const IndexPage = () => {
+  useEffect(function redirect() {
+    window.location.href = 'https://summit.nextflow.io/2024/boston/register/';
+  }, []);
+
   return (
     <>
       <Seo title="Sign up for SUMMIT 2024" image="/images/share-image.jpg" />
