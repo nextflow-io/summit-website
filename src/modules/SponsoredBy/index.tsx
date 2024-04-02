9d7b4c276855a40b9b5bbdc24142996dc579b245
Jake Broughton <jake.broughton@seqera.io>
2024-04-02 19:33:18 +0200

    Add Sponsors marquee

--- src/modules/SponsoredBy/index.tsx
+++ src/modules/SponsoredBy/index.tsx
@@ -1,61 +1,28 @@
+import React from "react";
 import clsx from "clsx";
 
-import LogoAWS from "./images/aws.svg";
-import LogoMicrosoft from "./images/microsoft.svg";
-import LogoPixelgen from "./images/pixelgen.svg";
-import LogoSeqera from "./images/seqera.svg";
-import LogoTileDB from "./images/tiledb.svg";
-import LogoZS from "./images/ZS.svg";
-import LogoNanopore from "./images/nanopore.svg";
-import LogoMemverge from "./images/memverge.svg";
-import LogoElement from "./images/element-biosciences.svg";
-import LogoQuilt from "./images/quilt.svg";
-import Sponsor from "./Sponsor";
+import sponsors from "@data/sponsors";
+import Marquee from "@components/Marquee";
 
 import styles from "./styles.module.css";
 
 type Props = {
-  alt?: boolean;
   className?: string;
 };
 
-const SectionSponsoredBy = ({ alt, className }: Props) => (
-  <section className={clsx("py-8", className)}>
-    {!alt && <h2 className="mb-2 uppercase text-center">Sponsored by</h2>}
-    <div className={clsx(styles.container, { [styles.alt]: alt })}>
-      <Sponsor>
-        <img src={LogoAWS.src} className="h-12" alt="AWS" />
-      </Sponsor>
-      <Sponsor>
-        <img src={LogoZS.src} className="h-14" alt="ZS" />
-      </Sponsor>
-      <div className="hidden lg:block w-full lg:hidden h-1" />
-      <Sponsor>
-        <img src={LogoTileDB.src} className="h-10" alt="TileDB" />
-      </Sponsor>
-      <Sponsor>
-        <img src={LogoMicrosoft.src} className="h-10" alt="Microsoft" />
-      </Sponsor>
-      <Sponsor>
-        <img src={LogoPixelgen.src} className="h-14" alt="Pixelgen" />
-      </Sponsor>
-      <Sponsor>
-        <img src={LogoNanopore.src} className="h-10" alt="Nanopore" />
-      </Sponsor>
-      <Sponsor>
-        <img src={LogoMemverge.src} className="h-10" alt="Memverge" />
-      </Sponsor>
-      <Sponsor>
-        <img src={LogoElement.src} className="h-10" alt="Element" />
-      </Sponsor>
-      <Sponsor>
-        <img src={LogoQuilt.src} className="h-10" alt="Quilt" />
-      </Sponsor>
-      <Sponsor>
-        <img src={LogoSeqera.src} className="h-10" alt="Seqera" />
-      </Sponsor>
-    </div>
+const SponsoredBy = ({ className }: Props) => (
+  <section>
+    <Marquee speed={50} className={className} href="/2024/boston/sponsors/">
+      <div className={clsx(styles.container)}>
+        <div className={styles.overlay} />
+        {sponsors.map((sponsor) => (
+          <div className="h-24 lg:h-40 max-ws px-10 flex items-center justify-center">
+            <img src={sponsor.image} className="h-10" alt="Seqera" />
+          </div>
+        ))}
+      </div>
+    </Marquee>
   </section>
 );
 
-export default SectionSponsoredBy;
+export default SponsoredBy;
