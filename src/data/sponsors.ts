51dfd0054816f62817483d7636008c5e4e69c8c2
Jake Broughton <jake.broughton@seqera.io>
2024-04-04 12:08:12 +0200

    Sponsor ordering

--- src/data/sponsors.ts
+++ src/data/sponsors.ts
@@ -16,8 +16,20 @@ const sponsors = await sanityClient.fetch(
   `*[_type == "sponsor"] | order(rank asc)`,
 );
 
-export default sponsors.map((sponsor) => ({
-  ...sponsor,
-  image: sponsor.image ? builder.image(sponsor.image).url() : null,
-  slug: sponsor.name.toLowerCase().replace(/ /g, "-"),
-})) as Sponsor[];
+const rankOrder = {
+  diamond: 1,
+  platinum: 2,
+  gold: 3,
+  silver: 4,
+  bronze: 5,
+};
+
+export default sponsors
+  .map((sponsor) => ({
+    ...sponsor,
+    image: sponsor.image ? builder.image(sponsor.image).url() : null,
+    slug: sponsor.name.toLowerCase().replace(/ /g, "-"),
+  }))
+  .sort((a, b) => {
+    return rankOrder[a.rank] - rankOrder[b.rank];
+  }) as Sponsor[];
