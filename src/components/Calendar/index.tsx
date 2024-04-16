14b6660504e78efe7fea2f8d60a386a3a3ad6157
Jake Broughton <jake.broughton@seqera.io>
2024-04-16 17:49:30 +0200

    Schedule updates

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,62 +1,102 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { HashNavigation } from "swiper/modules";
+
 import clsx from "clsx";
 import schedule from "@data/schedule";
 import sessions from "@data/sessions";
 
 import styles from "./styles.module.css";
-import { prettyDate } from "@utils/prettyDate";
+import { prettyDate, dateSlug } from "@utils/prettyDate";
 import { formatTime } from "./utils";
 import SessionCard from "@components/SessionCard";
 
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
 const Calendar = () => {
-  const [selectedDate, setSelectedDate] = useState(schedule[0].date);
-  const item = schedule.find((item) => item.date === selectedDate);
-  console.log(">>", selectedDate);
+  const [activeHash, setActiveHash] = useState("");
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+    setActiveHash(window.location.hash);
+    function updateCurrentPage() {
+      const hash = window.location.hash;
+      setActiveHash(hash);
+    }
+    addEventListener("hashchange", updateCurrentPage);
+    return function cleanup() {
+      removeEventListener("hashchange", updateCurrentPage);
+    };
+  }, []);
   return (
     <>
-      <nav className={styles.nav}>
-        {schedule.map((item) => (
-          <button
-            onClick={() => setSelectedDate(item.date)}
-            className={clsx({ [styles.active]: selectedDate === item.date })}
-          >
-            {prettyDate(item.date, false, true)}
-          </button>
-        ))}
+      <nav className={clsx(styles.nav, "container")}>
+        {schedule.map((item, i) => {
+          const hash = `#${dateSlug(item.date)}`;
+          console.log(">>", hash, activeHash, hash === activeHash);
+          return (
+            <a
+              key={i}
+              className={clsx({ [styles.active]: activeHash === hash })}
+              href={hash}
+            >
+              {prettyDate(item.date, false, true)}
+            </a>
+          );
+        })}
       </nav>
-      <div className={clsx(styles.col, "p-4")}>
-        <div className={styles.colItems}>
-          {item.timeSlots.map((slot) => {
-            const showRoomName = slot.rooms.length > 1;
-            const time = formatTime(slot.slotStart);
-            return (
-              <div className={styles.timeSlot}>
-                <div className={styles.time}>
-                  <div>
-                    {time.str}
-                    <span>{time.ampm}</span>
+      <Swiper
+        modules={[HashNavigation]}
+        hashNavigation={{
+          watchState: true,
+        }}
+        className={clsx(styles.calendar, "p-4")}
+      >
+        {schedule.map((item, i) => {
+          return (
+            <SwiperSlide
+              className={styles.page}
+              key={`page-${i}`}
+              data-hash={dateSlug(item.date)}
+            >
+              {item.timeSlots.map((slot, key) => {
+                const showRoomName = slot.rooms.length > 1;
+                const time = formatTime(slot.slotStart);
+                return (
+                  <div
+                    className={clsx(styles.item, "container")}
+                    key={`item-${key}`}
+                  >
+                    <div className={styles.time}>
+                      <div>
+                        {time.str}
+                        <span>{time.ampm}</span>
+                      </div>
+                    </div>
+                    <div className={styles.sessions}>
+                      {slot.rooms.map((room, i) => {
+                        const fullSession = sessions.find(
+                          (s) => s.id === room.session.id,
+                        );
+                        return (
+                          <SessionCard
+                            key={i}
+                            session={fullSession}
+                            hideTime
+                            showRoomName={showRoomName}
+                          />
+                        );
+                      })}
+                    </div>
                   </div>
-                </div>
-                <div className={styles.sessions}>
-                  {slot.rooms.map((room, i) => {
-                    const fullSession = sessions.find(
-                      (s) => s.id === room.session.id,
-                    );
-                    return (
-                      <SessionCard
-                        key={i}
-                        session={fullSession}
-                        hideTime
-                        showRoomName={showRoomName}
-                      />
-                    );
-                  })}
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </div>
+                );
+              })}
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </>
   );
 };
