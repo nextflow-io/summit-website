e09761f12ad3d237feca8cd824ee1e86b5d90c71
Phil Ewels <phil.ewels@seqera.io>
2024-05-14 21:56:07 +0200

    Barcelona - Why attend? Add BCN photos

--- src/modules/ImageSection/index.tsx
+++ src/modules/ImageSection/index.tsx
@@ -7,19 +7,25 @@ import img5 from "./images/5.jpg";
 import boston01 from "./images/boston-01.jpg";
 import boston02 from "./images/boston-02.jpg";
 import boston03 from "./images/boston-03.jpg";
+import barcelona01 from "./images/barcelona-01.jpg";
+import barcelona02 from "./images/barcelona-02.jpg";
+import barcelona03 from "./images/barcelona-03.jpg";
+import barcelona04 from "./images/barcelona-04.jpg";
 
 import styles from "./styles.module.css";
 
 type Props = {
   alt1?: boolean;
   alt2?: boolean;
+  alt3?: boolean;
   className?: string;
 };
 
-const ImageSection: React.FC<Props> = ({ alt1, alt2, className }) => {
+const ImageSection: React.FC<Props> = ({ alt1, alt2, alt3, className }) => {
   let images = [img1, img2];
   if (alt1) images = [img3, img4, img5];
   if (alt2) images = [boston01, boston02, boston03];
+  if (alt3) images = [barcelona01, barcelona02, barcelona03, barcelona04];
 
   return (
     <section className={styles.container}>
