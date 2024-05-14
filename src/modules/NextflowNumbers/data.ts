fa2f12d7dd44281627058b8b3f814332919fb4b6
Phil Ewels <phil.ewels@seqera.io>
2024-05-14 17:27:29 +0200

    Barcelona homepage: Add numbers

---
+++ src/modules/NextflowNumbers/data.ts
@@ -0,0 +1,56 @@
+export default {
+    boston: {
+        pharma_biotech: {
+            label: "Pharma & biotech",
+            value: 64
+        },
+        research_academia: {
+            label: "Research & academia",
+            value: 36
+        },
+        summit_in_person_attendees_prev_year: {
+            label: "Summit in-person attendees in 2023",
+            value: 145
+        },
+        summit_in_person_attendees_expected: {
+            label: "Summit in-person attendees expected in 2024",
+            value: 200
+        },
+        hackathon_in_person_attendees_prev_year: {
+            label: "Hackathon in-person attendees in 2023",
+            value: 50
+        },
+        hackathon_in_person_attendees_expected: {
+            label: "Hackathon in-person attendees expected in 2024",
+            value: 125
+        },
+    },
+    barcelona: {
+        pharma_biotech: {
+            label: "Pharma & biotech",
+            value: 43
+        },
+        research_academia: {
+            label: "Research & academia",
+            value: 57
+        },
+        summit_in_person_attendees_prev_year: {
+            label: "Summit in-person attendees in 2023",
+            value: 225
+        },
+        summit_in_person_attendees_expected: {
+            label: "Summit in-person attendees expected in 2024",
+            value: "400"
+        },
+        hackathon_in_person_attendees_prev_year: {
+            label: "Hackathon in-person attendees in 2023",
+            value: 100
+        },
+        hackathon_in_person_attendees_expected: {
+            label: "Hackathon in-person attendees expected in 2024",
+            value: "180"
+        },
+    }
+};
+
+
