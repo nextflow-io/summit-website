8ce0898fd8635bffacf928674c226541258bf9d3
Jake Broughton <jake.broughton@seqera.io>
2024-05-24 10:18:23 +0200

    Add YT video to session page

--- src/components/YouTubeVideo/index.tsx
+++ src/components/YouTubeVideo/index.tsx
@@ -3,15 +3,28 @@ import styles from "./styles.module.css";
 
 type Props = {
   id?: string;
+  url?: string;
   listId?: string;
   className?: string;
 };
 
-const YouTubeVideo: React.FC<Props> = ({ id, listId, className }) => {
+const YouTubeVideo: React.FC<Props> = ({ id, url, listId, className }) => {
   let videoSource = `https://www.youtube.com/embed/${id}`;
 
   if (listId) {
     videoSource += `?list=${listId}`;
+  }
+
+  if (url) {
+    if (url.includes("embed")) {
+      videoSource = url;
+    } else if (url.includes("v=")) {
+      const videoId = url.split("v=")[1];
+      videoSource = `https://www.youtube.com/embed/${videoId}`;
+    } else {
+      const videoId = url.split("/").pop();
+      videoSource = `https://www.youtube.com/embed/${videoId}`;
+    }
   }
 
   return (
