59c690401bf71fe61e133ee1aca3a26d01f785aa
Jake Broughton <jake.broughton@seqera.io>
2024-05-30 13:58:35 +0200

    Fix number scrolling

--- src/modules/NextflowNumbers/index.tsx
+++ src/modules/NextflowNumbers/index.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import CountUp from "@components/CountUp";
-import LazyLoader from "@components/LazyLoader";
 import data from "./data";
 
 type ItemProps = {
@@ -32,13 +31,15 @@ const Item: React.FC<ItemProps> = ({
     }
   });
   let cn = "w-full xs:w-1/2 flex-auto min-w-[100px] px-8 py-4 md:px-4";
-  if (smaller) cn = "w-full xs:w-1/2 md:w-full flex-auto min-w-[100px] px-8 py-4 md:px-4";
+  if (smaller)
+    cn = "w-full xs:w-1/2 md:w-full flex-auto min-w-[100px] px-8 py-4 md:px-4";
   return (
     <CountUp
       start={ready ? 0 : null}
       end={value}
       duration={duration}
       separator=","
+      enableScrollSpy
     >
       {({ countUpRef }) => (
         <div className={cn}>
@@ -63,48 +64,86 @@ const Numbers: React.FC<Props> = (props = {}) => {
   const [ready, setReady] = useState(false);
   const { className, location = "boston" } = props;
   return (
-    <LazyLoader onView={() => setReady(true)}>
-      <section className={className}>
-        <div className="container">
-          <div className="flex flex-wrap">
-            <div className="w-full lg:w-1/3 lg:pr-12">
-              <h3 className="text-5xl font-display font-semibold text-center lg:text-left mb-12 px-4 lg:px-0 lg:mb-4">
-                Nextflow Summit
-                <br className="hidden lg:block" /> in numbers
-              </h3>
-            </div>
-            <div className="w-full lg:w-2/3 flex flex-wrap">
-              <div className="md:border-r border-1 border-brand-900 w-full md:w-2/6 md:pr-6">
-                <div className="h-full w-full flex items-start flex-wrap -m-4">
-                  <Item ready={ready} value={data[location]['pharma_biotech'].value} percent smaller>
-                    { data[location]['pharma_biotech'].label }
-                  </Item>
-                  <Item ready={ready} value={data[location]['research_academia'].value} percent smaller>
-                    { data[location]['research_academia'].label }
-                  </Item>
-                </div>
+    <section className={className}>
+      <div className="container">
+        <div className="flex flex-wrap">
+          <div className="w-full lg:w-1/3 lg:pr-12">
+            <h3 className="text-5xl font-display font-semibold text-center lg:text-left mb-12 px-4 lg:px-0 lg:mb-4">
+              Nextflow Summit
+              <br className="hidden lg:block" /> in numbers
+            </h3>
+          </div>
+          <div className="w-full lg:w-2/3 flex flex-wrap">
+            <div className="md:border-r border-1 border-brand-900 w-full md:w-2/6 md:pr-6">
+              <div className="h-full w-full flex items-start flex-wrap -m-4">
+                <Item
+                  ready={ready}
+                  value={data[location]["pharma_biotech"].value}
+                  percent
+                  smaller
+                >
+                  {data[location]["pharma_biotech"].label}
+                </Item>
+                <Item
+                  ready={ready}
+                  value={data[location]["research_academia"].value}
+                  percent
+                  smaller
+                >
+                  {data[location]["research_academia"].label}
+                </Item>
               </div>
-              <div className="md:w-4/6 md:pl-12 pt-8 md:pt-0">
-                <div className="h-full w-full flex items-start flex-wrap -m-4">
-                  <Item ready={ready} value={data[location]['summit_in_person_attendees_prev_year'].value}>
-                    { data[location]['summit_in_person_attendees_prev_year'].label }
-                  </Item>
-                  <Item ready={ready} value={data[location]['summit_in_person_attendees_expected'].value} plus>
-                    { data[location]['summit_in_person_attendees_expected'].label }
-                  </Item>
-                  <Item ready={ready} value={data[location]['hackathon_in_person_attendees_prev_year'].value}>
-                    { data[location]['hackathon_in_person_attendees_prev_year'].label }
-                  </Item>
-                  <Item ready={ready} value={data[location]['hackathon_in_person_attendees_expected'].value} plus>
-                    { data[location]['hackathon_in_person_attendees_expected'].label }
-                  </Item>
-                </div>
+            </div>
+            <div className="md:w-4/6 md:pl-12 pt-8 md:pt-0">
+              <div className="h-full w-full flex items-start flex-wrap -m-4">
+                <Item
+                  ready={ready}
+                  value={
+                    data[location]["summit_in_person_attendees_prev_year"].value
+                  }
+                >
+                  {data[location]["summit_in_person_attendees_prev_year"].label}
+                </Item>
+                <Item
+                  ready={ready}
+                  value={
+                    data[location]["summit_in_person_attendees_expected"].value
+                  }
+                  plus
+                >
+                  {data[location]["summit_in_person_attendees_expected"].label}
+                </Item>
+                <Item
+                  ready={ready}
+                  value={
+                    data[location]["hackathon_in_person_attendees_prev_year"]
+                      .value
+                  }
+                >
+                  {
+                    data[location]["hackathon_in_person_attendees_prev_year"]
+                      .label
+                  }
+                </Item>
+                <Item
+                  ready={ready}
+                  value={
+                    data[location]["hackathon_in_person_attendees_expected"]
+                      .value
+                  }
+                  plus
+                >
+                  {
+                    data[location]["hackathon_in_person_attendees_expected"]
+                      .label
+                  }
+                </Item>
               </div>
             </div>
           </div>
         </div>
-      </section>
-    </LazyLoader>
+      </div>
+    </section>
   );
 };
 
