eda0b8512c3c602e99cc34248075ee2680888a15
Jake Broughton <jake.broughton@seqera.io>
2024-08-20 16:50:31 +0200

    Use localStorage for UTM

--- src/utils/utm.ts
+++ src/utils/utm.ts
@@ -1,4 +1,4 @@
-import { writeCookie, readCookie } from "./cookies"
+import { writeCookie, readCookie } from "./cookies";
 
 const utm_keys = [
   "utm_source",
@@ -6,7 +6,7 @@ const utm_keys = [
   "utm_medium",
   "utm_content",
   "utm_term",
-]
+];
 
 type UTMValues = {
   utm_source?: string;
@@ -14,49 +14,67 @@ type UTMValues = {
   utm_medium?: string;
   utm_content?: string;
   utm_term?: string;
-}
+};
 
 function getUTMparams(): UTMValues {
-  const urlParams = new URLSearchParams(window.location.search)
-  const params = {}
+  const urlParams = new URLSearchParams(window.location.search);
+  const params = {};
   for (const key of utm_keys) {
-    let val = urlParams.get(key)
-    if (val) params[key] = val
+    let val = urlParams.get(key);
+    if (val) params[key] = val;
   }
-  return params
+  return params;
 }
 
 function getUTMcookies(): UTMValues {
-  const cookies = {}
+  const cookies = {};
   for (const key of utm_keys) {
-    let val = readCookie(key)
-    if (val) cookies[key] = val
+    let val = readCookie(key);
+    if (val) cookies[key] = val;
   }
-  return cookies
+  return cookies;
+}
+
+function getUTMlocalStorage(): UTMValues {
+  const localStorage = {};
+  for (const key of utm_keys) {
+    let val = window.localStorage.getItem(key);
+    if (val) localStorage[key] = val;
+  }
+  return localStorage;
 }
 
 function getUTM(): UTMValues {
-  const utmParams = getUTMparams()
-  const utmCookies = getUTMcookies()
-  return { ...utmCookies, ...utmParams }
+  const utmParams = getUTMparams();
+  const utmCookies = getUTMcookies();
+  const utmLocalStorage = getUTMlocalStorage();
+  return { ...utmCookies, ...utmLocalStorage, ...utmParams };
 }
 
 function setUTMcookies() {
-  const utmParams = getUTMparams()
+  const utmParams = getUTMparams();
   for (const [key, value] of Object.entries(utmParams)) {
-    if (!value) continue
-    writeCookie(key, value, 30)
+    if (!value) continue;
+    writeCookie(key, value, 30);
+  }
+}
+
+function setUTMlocalStorage() {
+  const utmParams = getUTMparams();
+  for (const [key, value] of Object.entries(utmParams)) {
+    if (!value) continue;
+    localStorage.setItem(key, value);
   }
 }
 
 function setUTMfields() {
-  const utmValues = getUTM()
+  const utmValues = getUTM();
   for (const [key, value] of Object.entries(utmValues)) {
-    if (!value) continue
-    let utmField = document.querySelector(`input[name="${key}"]`)
-    if (!utmField) continue
-    utmField.setAttribute("value", value)
+    if (!value) continue;
+    let utmField = document.querySelector(`input[name="${key}"]`);
+    if (!utmField) continue;
+    utmField.setAttribute("value", value);
   }
 }
 
-export { setUTMcookies, setUTMfields, getUTM }
+export { setUTMcookies, setUTMlocalStorage, setUTMfields, getUTM };
