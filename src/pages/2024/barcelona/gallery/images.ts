3759723bfea108c841cb75f52d972ccf2a720168
dana-seqera <dana.kim@seqera.io>
2025-01-23 17:35:08 -0500

    remove extra images gallery

--- src/pages/2024/barcelona/gallery/images.ts
+++ src/pages/2024/barcelona/gallery/images.ts
@@ -1,19 +1,19 @@
-import img1 from "./images/summit-barcelona-1.png";
-import img2 from "./images/summit-barcelona-2.png";
-import img3 from "./images/summit-barcelona-3.png";
-import img4 from "./images/summit-barcelona-4.png";
-import img5 from "./images/summit-barcelona-5.png";
-import img6 from "./images/summit-barcelona-6.png";
-import img7 from "./images/summit-barcelona-7.png";
-import img8 from "./images/summit-barcelona-8.png";
-import img9 from "./images/summit-barcelona-9.png";
-import img10 from "./images/summit-barcelona-10.png";
-import img11 from "./images/summit-barcelona-11.png";
-import img12 from "./images/summit-barcelona-12.png";
-import img13 from "./images/summit-barcelona-13.png";
-import img14 from "./images/summit-barcelona-14.png";
-import img15 from "./images/summit-barcelona-15.jpg";
-import img16 from "./images/summit-barcelona-16.jpg";
+import img1 from "@images/photos/barcelona-2024/summit-barcelona-1.png";
+import img2 from "@images/photos/barcelona-2024/summit-barcelona-2.png";
+import img3 from "@images/photos/barcelona-2024/summit-barcelona-3.png";
+import img4 from "@images/photos/barcelona-2024/summit-barcelona-4.png";
+import img5 from "@images/photos/barcelona-2024/summit-barcelona-5.png";
+import img6 from "@images/photos/barcelona-2024/summit-barcelona-6.png";
+import img7 from "@images/photos/barcelona-2024/summit-barcelona-7.png";
+import img8 from "@images/photos/barcelona-2024/summit-barcelona-8.png";
+import img9 from "@images/photos/barcelona-2024/summit-barcelona-9.png";
+import img10 from "@images/photos/barcelona-2024/summit-barcelona-10.png";
+import img11 from "@images/photos/barcelona-2024/summit-barcelona-11.png";
+import img12 from "@images/photos/barcelona-2024/summit-barcelona-12.png";
+import img13 from "@images/photos/barcelona-2024/summit-barcelona-13.png";
+import img14 from "@images/photos/barcelona-2024/summit-barcelona-14.png";
+import img15 from "@images/photos/barcelona-2024/summit-barcelona-15.jpg";
+import img16 from "@images/photos/barcelona-2024/summit-barcelona-16.jpg";
 
 
 export {
