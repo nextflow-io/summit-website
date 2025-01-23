7a15b69094edc08b8927fc6368019568f621f0a6
dana-seqera <dana.kim@seqera.io>
2025-01-23 14:02:47 -0500

    footer wip

--- src/layouts/Footer/index.tsx
+++ src/layouts/Footer/index.tsx
@@ -1,47 +1,93 @@
 import React from "react";
-
-import Github from "@icons/Github";
-import Linkedin from "@icons/Linkedin";
-import Twitter from "@icons/Twitter";
-
+import SeqeraLogo from "./SeqeraLogo";
+import FooterLogo from "./FooterLogo";
+import Subscribe from "@modules/Subscribe";
 const currentYear = new Date().getFullYear();
 
 const Footer: React.FC = () => {
   return (
     <footer id="footer">
-      <div className="container flex items-center justify-between py-8 text-white opacity-50">
-        <div className="text-xs">{`© ${currentYear} Seqera`}</div>
-        <nav>
-          <ul className="flex">
-            <li>
-              <a
-                href="https://twitter.com/seqeralabs"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Twitter />
-              </a>
-            </li>
-            <li className="mx-6">
-              <a
-                href="https://github.com/seqeralabs"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Github />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.linkedin.com/company/14065390/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Linkedin />
-              </a>
-            </li>
-          </ul>
+      <div className="wrapper py-8 text-white monospace">
+        <nav className="flex flex-col lg:flex-row lg:justify-between">
+          <div className="w-full max-w-[500px] lg:pr-10 mb-10 lg:mb-0">
+            <Subscribe />
+          </div>
+          <div className="w-full flex lg:justify-center">
+            <div className="grid grid-cols-2 lg:grid-cols-4 lg:gap-x-12 lg:content-center whitespace-nowrap">
+              <ul className="flex flex-col">
+                <p className="mb-6">Pages</p>
+                <li>
+                  <a href="/past-events">Past Events</a>
+                </li>
+                <li>
+                  <a href="/register">Register</a>
+                </li>
+                <li>
+                  <a href="/call-for-abstracts">Call for Abstracts</a>
+                </li>
+              </ul>
+              <ul className="flex flex-col ">
+                <p className="mb-6">Boston</p>
+                <li>Coming Soon</li>
+              </ul>
+              <ul className="flex flex-col ">
+                <p className="mb-6">Barcelona</p>
+                <li>Coming Soon</li>
+              </ul>
+              <ul className="flex flex-col">
+                <p className="mb-6">Follow</p>
+                <li>
+                  <a
+                    href="https://twitter.com/seqeralabs"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    X
+                  </a>
+                </li>
+                <li className="">
+                  <a
+                    href="https://github.com/seqeralabs"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Github
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.youtube.com/channel/UCBDzWsz3bDsxZblHbmGxsww"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Youtube
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.linkedin.com/company/14065390/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Linkedin
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </nav>
+      </div>
+      <div className="wrapper mb-16">
+        <div className="mt-10 mb-6">
+          <FooterLogo />
+        </div>
+        <div className="flex flex-row justify-between">
+          <div className="flex flex-row">
+            <small>By</small>
+            <div className="pl-2 max-w-[150px]"><SeqeraLogo /></div>
+          </div>
+          <div className="monospace">{`© ${currentYear} Seqera`}</div>
+        </div>
       </div>
     </footer>
   );
--- src/modules/Subscribe/index.tsx
+++ src/modules/Subscribe/index.tsx
@@ -7,17 +7,20 @@ import styles from "./styles.module.css";
 
 const SectionSubscribe: React.FC<Props> = () => {
   return (
-    <section className="container text-center pb-30">
-      <h4 className="h00">Subscribe to our newsletter</h4>
-      <div className="blockquote mt-2 mb-6">
+    <section className="">
+      <p className="">
         Register and be the first to know about all our news and updates.
-      </div>
+      </p>
+      <div>
       <HubspotForm
         className={styles.form}
         formID="a3ee7c65-a905-451d-aba6-cf125a3b89b3"
         linkedInEventID={13682684}
         gtmEventID="gtm_newsletter"
       />
+      </div>
+      <small>We respect your data. By submitting this form, you agree that we may use ​this information in accordance with our Privacy Policy.</small>
+
     </section>
   );
 };
