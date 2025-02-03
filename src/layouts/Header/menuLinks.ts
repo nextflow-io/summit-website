dfef731f91417676144bffebf1620fe552a93cce
dana-seqera <dana.kim@seqera.io>
2025-02-02 20:36:51 -0500

    nav updates

--- src/layouts/Header/menuLinks.ts
+++ src/layouts/Header/menuLinks.ts
@@ -4,8 +4,8 @@ const menuLinks = [
     url: "/2025/boston",
     dropdowns: [
       {
-        name: "Agenda",
-        url: "/2025/boston/agenda",
+        name: "Overview",
+        url: "/2025/boston/",
       },
       {
         name: "Travel",
@@ -16,25 +16,33 @@ const menuLinks = [
         url: "/2025/boston/training",
       },
       {
-        name: "Hackathon",
-        url: "/2025/boston/hackathon",
+        name: "Agenda",
+        url: "/2025/boston/agenda",
       },
+      // {
+      //   name: "Hackathon",
+      //   url: "/2025/boston/hackathon",
+      // },
       {
         name: "Speakers",
         url: "/2025/boston/speakers",
       },
-      {
-        name: "Posters",
-        url: "/2025/boston/posters",
-      },
+      // {
+      //   name: "Posters",
+      //   url: "/2025/boston/posters",
+      // },
       {
         name: "Sponsors",
         url: "/2025/boston/sponsors",
       },
       {
-        name: "Gallery",
-        url: "/2025/boston/gallery",
+        name: "Why Attend",
+        url: "/2025/boston/why-attend",
       },
+      // {
+      //   name: "Gallery",
+      //   url: "/2025/boston/gallery",
+      // },
     ],
   },
   {
@@ -42,8 +50,8 @@ const menuLinks = [
     url: "/2025/barcelona",
     dropdowns: [
       {
-        name: "Agenda",
-        url: "/2025/barcelona/agenda",
+        name: "Overview",
+        url: "/2025/barcelona",
       },
       {
         name: "Travel",
@@ -54,25 +62,29 @@ const menuLinks = [
         url: "/2025/barcelona/training",
       },
       {
-        name: "Hackathon",
-        url: "/2025/barcelona/hackathon",
+        name: "Agenda",
+        url: "/2025/barcelona/agenda",
       },
+      // {
+      //   name: "Hackathon",
+      //   url: "/2025/barcelona/hackathon",
+      // },
       {
         name: "Speakers",
         url: "/2025/barcelona/speakers",
       },
-      {
-        name: "Posters",
-        url: "/2025/barcelona/posters",
-      },
+      // {
+      //   name: "Posters",
+      //   url: "/2025/barcelona/posters",
+      // },
       {
         name: "Sponsors",
         url: "/2025/barcelona/sponsors",
       },
-      {
-        name: "Gallery",
-        url: "/2025/barcelona/gallery",
-      },
+      // {
+      //   name: "Gallery",
+      //   url: "/2025/barcelona/gallery",
+      // },
     ],
   },
   {
---
+++ src/pages/2025/boston/training/qa.ts
@@ -0,0 +1,35 @@
+const qa = [
+    {
+      question: "Do I need prior experience with Nextflow to attend the training?",
+      answer:
+        `No prior experience is needed to attend the training. THIS IS PLACEHOLDER COPY THAT NEEDS TO BE UPDATED`,
+    },
+    {
+      question: "What types of training sessions are offered at the Nextflow Summit?",
+      answer:
+        "THIS IS PLACEHOLDER COPY THAT NEEDS TO BE UPDATED"
+    },
+    {
+      question: "How can I register for a training session?",
+      answer:
+        "THIS IS PLACEHOLDER COPY THAT NEEDS TO BE UPDATED"
+    },
+    {
+      question: "What is the duration and format of the training sessions?",
+      answer:
+        "THIS IS PLACEHOLDER COPY NEEDS TO BE UPDATED"
+    },
+    {
+      question: "Are training materials provided, and will recordings be available?",
+      answer:
+        "THIS IS PLACEHOLDER COPY THAT NEEDS TO BE UPDATED"
+    },
+    {
+        question: "Is there an additional fee to attend the training sessions?",
+        answer:
+          "THIS IS PLACEHOLDER COPY THAT NEEDS TO BE UPDATED."
+      }
+  ];
+  
+  export default qa;
+  
