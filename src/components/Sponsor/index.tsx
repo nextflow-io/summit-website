774177e153a84db2a8b88c566b87df44245b8b55
dana-seqera <dana.kim@seqera.io>
2025-02-03 12:40:09 -0500

    sponsor page updates

--- src/components/Sponsor/index.tsx
+++ src/components/Sponsor/index.tsx
@@ -16,55 +16,33 @@ const Sponsor = ({ sponsor, location }) => {
   let rank = ranks[sponsor.rank];
   if (location === "barcelona") rank = ranks[sponsor.rankBarcelona];
   return (
-
-    <Box className="">
-    <SubSection className="flex flex-col justify-between min-h-[350px]">
-      <div className="mb-4  pb-4 border-b border-b-nextflow w-full flex flex-row justify-between">
-      <div className={clsx(styles.rank, styles[rank])}>{rank} sponsor of Nextflow Summit 2025</div>
-      </div>
-      <div>
-      <img
-              src={sponsor.image}
-              title={sponsor.name}
-              className="max-w-[200px] max-h-[55px] min-w-[100px] mr-4 mb-4"
-            />
-       <PortableText
-          className="mt-4 monospace text-sm"
-          value={sponsor.description}
-        />
-    </div>
-    <div className="mt-4 pt-4 border-t border-t-nextflow w-full flex flex-row justify-between items-center">
-        <h5 className="h5"><a href={sponsor.url}>Find out more</a></h5>
-        <ArrowUpRight />
-      </div>
-    </SubSection>
-  </Box>
-    // <div className="p-2">
-    //   <a
-    //     href={`/2025/${location}/sponsors/${sponsor.slug}/`}
-    //     className={clsx(
-    //       "block p-8 sm:p-12",
-    //       "transition-all border border-nextflow text-left",
-    //     )}
-    //   >
-    //     <div className="flex justify-between items-start flex-col sm:flex-row">
-    //       <h2 className="flex-auto">
-    //         <img
-    //           src={sponsor.image}
-    //           title={sponsor.name}
-    //           className="max-w-[200px] max-h-[55px] min-w-[100px] mr-4 mb-4"
-    //         />
-    //       </h2>
-    //       <div className={clsx(styles.rank, styles[rank])}>{rank} sponsor</div>
-    //     </div>
-    //     <PortableText
-    //       defaultStyle
-    //       light
-    //       className="mt-4 monospace"
-    //       value={sponsor.description}
-    //     />
-    //   </a>
-    // </div>
+    <Box className="flex flex-col justify-between min-h-[350px]">
+      <SubSection className="">
+        <div className="mb-4 pb-4 border-b border-b-nextflow w-full flex flex-row justify-between">
+          <div className={clsx(styles.rank, styles[rank], "monospace")}>
+            {rank} Sponsor of Nextflow Summer 2025
+          </div>
+        </div>
+        <div>
+          <img
+            src={sponsor.image}
+            title={sponsor.name}
+            className="max-w-[200px] max-h-[55px] min-w-[100px] mr-4 my-10" 
+          />
+          <PortableText
+            className="mt-4 monospace text-sm"
+            value={sponsor.description}
+          />
+        </div>
+      </SubSection>
+      <div className={clsx(styles.ctaLink, "relative mt-8 pt-4 border-t border-t-nextflow w-full flex flex-row justify-between items-center")}>
+          <h5 className="h5">
+            Find out more
+          </h5>
+          <ArrowUpRight />
+          <a className="absolute top-0 left-0 right-0 bottom-0 w-full h-full" href={sponsor.url}></a>
+        </div>
+    </Box>
   );
 };
 
