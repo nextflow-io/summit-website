15648979995d29c7c267824d927a1ea1a9ff0d5f
dana-seqera <dana.kim@seqera.io>
2025-02-02 23:02:52 -0500

    key dates wip

---
+++ src/components/KeyDates/keyDates.ts
@@ -0,0 +1,31 @@
+const keyDates = [
+  {
+    date: "Feb 10",
+    content: [
+      {
+        info: "Call for abstracts opens",
+        url: "/call-for-abstracts",
+      },
+      {
+        info: "Early bird 20% off registration starts",
+        url: "/register",
+      },
+    ],
+  },
+  {
+    date: "March 14",
+    content: [
+      {
+        info: "Call for abstracts closes",
+      },
+      {
+        info: "Early bird 20% off registration ends",
+      },
+      {
+        info: "Standard pricing starts (until supplies last)",
+      },
+    ],
+  },
+];
+
+export default keyDates;
