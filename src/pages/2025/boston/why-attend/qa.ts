e003c0a192fe172f2df9f7abcd6a0cb4ab49e5b7
dana-seqera <dana.kim@seqera.io>
2025-02-02 21:25:11 -0500

    why attend wip

---
+++ src/pages/2025/boston/why-attend/qa.ts
@@ -0,0 +1,35 @@
+const qa = [
+  {
+    question: "Need help convincing your manager? ",
+    answer: `Need help getting approval to attend the Nextflow Summit? We’ve got you covered. To make it easier to explain the benefits of attending, we’ve prepared a template letter for you. Just edit it to suit your needs and make your case. Take the first step toward joining us—download the template now and secure your spot! <a href="">Download the template </a>
+`,
+  },
+  {
+    question: "Are there recommended hotels near the event venue?",
+    answer:
+      "Please see above for our hotel recommendations near the event venue.",
+  },
+  {
+    question: "Who typically attends the summit, and what networking opportunities are available?",
+    answer: "THIS IS PLACEHOLDER COPY THAT NEEDS TO BE UPDATED",
+  },
+  {
+    question: "What skills or knowledge can I expect to gain by attending?",
+    answer: "THIS IS PLACEHOLDER COPY NEEDS TO BE UPDATED",
+  },
+  {
+    question: "Will there be opportunities to interact with Nextflow experts and developers?",
+    answer: "THIS IS PLACEHOLDER COPY THAT NEEDS TO BE UPDATED",
+  },
+  {
+    question:
+      "What are the key topics or themes covered during the summit?",
+    answer: "THIS IS PLACEHOLDER COPY THAT NEEDS TO BE UPDATED.",
+  },
+  {
+    question: "How will attending the summit benefit my professional growth or research?",
+    answer: "THIS IS PLACEHOLDER COPY THAT NEEDS TO BE UPDATED",
+  },
+];
+
+export default qa;
