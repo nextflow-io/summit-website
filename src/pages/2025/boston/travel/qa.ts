274364f09c7bf710486fb74ca46f8f8c3eafe6ef
dana-seqera <dana.kim@seqera.io>
2025-02-02 20:59:11 -0500

    travel wip

---
+++ src/pages/2025/boston/travel/qa.ts
@@ -0,0 +1,40 @@
+const qa = [
+    {
+      question: "What is the best way to travel to Boston for the event?",
+      answer:
+        `THIS IS PLACEHOLDER COPY THAT NEEDS TO BE UPDATED`,
+    },
+    {
+      question: "Are there recommended hotels near the event venue?",
+      answer:
+        "Please see above for our hotel recommendations near the event venue."
+    },
+    {
+      question: "How can I register for a training session?",
+      answer:
+        "THIS IS PLACEHOLDER COPY THAT NEEDS TO BE UPDATED"
+    },
+    {
+      question: "What transportation options are available in Boston?",
+      answer:
+        "THIS IS PLACEHOLDER COPY NEEDS TO BE UPDATED"
+    },
+    {
+      question: "Do I need a visa to travel to Boston?",
+      answer:
+        "THIS IS PLACEHOLDER COPY THAT NEEDS TO BE UPDATED"
+    },
+    {
+        question: "Are there any discounted rates or travel partnerships available for attendees?",
+        answer:
+          "THIS IS PLACEHOLDER COPY THAT NEEDS TO BE UPDATED."
+    },
+    {
+      question: "Can I add training after I complete my registration??",
+      answer:
+        "THIS IS PLACEHOLDER COPY THAT NEEDS TO BE UPDATED"
+    },
+  ];
+  
+  export default qa;
+  
