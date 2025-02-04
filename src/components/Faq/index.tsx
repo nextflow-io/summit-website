42a58a5fc5eb5b4b39a8d842bfade67b811796c9
dana-seqera <dana.kim@seqera.io>
2025-02-03 22:48:23 -0500

    fix faq dropdown

--- src/components/Faq/index.tsx
+++ src/components/Faq/index.tsx
@@ -12,6 +12,45 @@ type FaqProps = {
   onClick?: boolean;
 };
 
+const DropDownItem = ({ question, answer, isOpen, onClick }) => {
+  const contentHeight = useRef();
+  return (
+    <div className={`border-b first:border-t py-10`}>
+      <button
+        className={`flex flex-row justify-between items-start w-full hover:text-nextflow transition-all duration-500 ease-in-out ${isOpen ? "active text-nextflow" : ""}`}
+        onClick={onClick}
+      >
+        <h5 className="h5 text-left pr-8">{question}</h5>
+
+        <div
+          className={`h5 leading-none duration-500 ease-in-out transition-all duration-500 ease-in-out ${isOpen ? "h2 active origin-center rotate-45 " : "origin-center"}`}
+        >
+          +
+        </div>
+      </button>
+
+      <div
+        ref={contentHeight}
+        className={`transition-all duration-500 ease-in-out`}
+        style={
+          isOpen
+            ? {
+                height: contentHeight?.current?.scrollHeight,
+                marginTop: "2rem",
+                marginBottom: "2rem",
+              }
+            : { height: "0px", overflow: "hidden" }
+        }
+      >
+        <div
+          className="monospace text-body"
+          dangerouslySetInnerHTML={{ __html: answer }}
+        />
+      </div>
+    </div>
+  );
+};
+
 const Faq = ({ className, data, title, question, answer }: FaqProps) => {
   const [activeIndex, setActiveIndex] = useState(null);
   const contentHeight = useRef();
@@ -25,44 +64,20 @@ const Faq = ({ className, data, title, question, answer }: FaqProps) => {
         <div className="w-full mb-6 md:mb-0 md:sticky md:top-40 self-start">
           <h2 className="h1 max-w-[500px] relative z-20">More Information</h2>
           <div className="mt-[-50px] ml-[-50px] z-0">
-           <FaqBar/>
+            <FaqBar />
           </div>
         </div>
         <div className="w-full">
           {data.map(({ question, answer }, index) => {
             return (
-              <div key={index} className={`border-b first:border-t py-10`}>
-                <button
-                  onClick={() => handleItemClick(index)}
-                  className={`flex flex-row justify-between items-start w-full hover:text-nextflow transition-all duration-500 ease-in-out ${activeIndex === index ? "active  text-nextflow" : ""}`}
                 >
-                  <h5 className="h5 text-left pr-8">{question}</h5>
-                  <div
-                    className={`h5 leading-none transition-transform duration-500 ease-in-out ${activeIndex === index ? "h2 active origin-center rotate-45" : "origin-center"}`}
-                  >
-                    +
-                  </div>
-                </button>
-
-                <div
-                  ref={contentHeight}
-                  className={`transition-all duration-500 ease-in-out`}
-                  style={
-                    activeIndex === index
-                      ? {
-                          height: contentHeight?.current?.scrollHeight,
-                          marginTop: "2rem",
-                          marginBottom: "2rem",
-                        }
-                      : { height: "0px", overflow: "hidden" }
-                  }
-                >
-                  <div
-                    className="monospace text-body"
-                    dangerouslySetInnerHTML={{ __html: answer }}
-                  />
-                </div>
-              </div>
+              <DropDownItem
+                key={index}
+                question={question}
+                answer={answer}
+                isOpen={activeIndex === index}
+                onClick={() => handleItemClick(index)}
+              />
             );
           })}
         </div>
