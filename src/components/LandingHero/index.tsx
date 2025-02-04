d1eee641e30dda793a0325bed9019b0a3fa2b11a
dana-seqera <dana.kim@seqera.io>
2025-02-04 15:51:15 -0500

    v1 animation as css

--- src/components/LandingHero/index.tsx
+++ src/components/LandingHero/index.tsx
@@ -31,15 +31,8 @@ const LandingHero = ({
   price,
 }: HeroProps) => {
   return (
-    <section
-      className={clsx(styles.landingHero, "container h-screen relative")}
-    >
-      {/* <RedBar className="max-w-[542px]"/>
-      // <GreenBar className="max-w-[542px]"/>
-      <BlueBar className="max-w-[542px]"/> */}
-
-
-       <div className={clsx(styles.colorBarWrapper, "top-[0%] right-[15%]")}>
+    <div className="relative w-full h-full">
+      <div className={clsx(styles.colorBarWrapper, "top-[0%] right-[15%]")}>
         <div className={clsx(styles.colorBar, styles["red"], "")}>
           <div></div>
         </div>
@@ -57,73 +50,76 @@ const LandingHero = ({
         </div>
       </div>
 
-      <div className={clsx(styles.colorBarWrapper, "bottom-[5%] right-[10%]")}>
+      <div className={clsx(styles.colorBarWrapper, "bottom-[5%] right-[00%]")}>
         <div className={clsx(styles.colorBar, styles["red"], "")}>
           <div></div>
         </div>
       </div>
 
+      <section
+        className={clsx(styles.landingHero, "container h-screen relative")}
+      >
+        <div className="flex flex-col md:flex-row md:justify-between h-full relative z-10">
+          <div className="w-full h-full mt-10 md:mt-0 mb-10 mb:mb-0 md:pr-10 max-w-[750px]">
+            <h1 className="h0 mb-4">{title}</h1>
+            <div
+              className={clsx(
+                styles.landingLogo,
+                "flex flex-row items-center mb-16",
+              )}
+            >
+              <p className="h3 mr-4">by</p> <SeqeraLogo />
+            </div>
+            <p
+              className="monospace max-w-[680px]"
+              dangerouslySetInnerHTML={{ __html: content }}
+            />
 
-      <div className="flex flex-col md:flex-row md:justify-between h-full relative z-10">
-        <div className="w-full h-full mt-10 md:mt-0 mb-10 mb:mb-0 md:pr-10 max-w-[750px]">
-          <h1 className="h0 mb-4">{title}</h1>
+            <div className="inline-flex flex-col md:flex-row md:items-center">
+              {ctaText1 && (
+                <Button className="mt-10 relative" white arrowAfter>
+                  {ctaLink1 && (
+                    <a className="absolute w-full h-full" href={ctaLink1}></a>
+                  )}{" "}
+                  {ctaText1}
+                </Button>
+              )}
+
+              {ctaText2 && (
+                <Button className="md:ml-10 mt-6 md:mt-10 relative" arrowBefore>
+                  {ctaLink2 && (
+                    <a className="absolute w-full h-full" href={ctaLink2}></a>
+                  )}{" "}
+                  {ctaText2}
+                </Button>
+              )}
+            </div>
+          </div>
           <div
             className={clsx(
-              styles.landingLogo,
-              "flex flex-row items-center mb-16",
+              styles.rightWrapper,
+              "h-full flex flex-col justify-between md:text-right",
             )}
           >
-            <p className="h3 mr-4">by</p> <SeqeraLogo />
-          </div>
-          <p
-            className="monospace max-w-[680px]"
-            dangerouslySetInnerHTML={{ __html: content }}
-          />
-
-          <div className="inline-flex flex-col md:flex-row md:items-center">
-            {ctaText1 && (
-              <Button className="mt-10 relative" white arrowAfter>
-                {ctaLink1 && (
-                  <a className="absolute w-full h-full" href={ctaLink1}></a>
-                )}{" "}
-                {ctaText1}
-              </Button>
-            )}
-
-            {ctaText2 && (
-              <Button className="md:ml-10 mt-6 md:mt-10 relative" arrowBefore>
-                {ctaLink2 && (
-                  <a className="absolute w-full h-full" href={ctaLink2}></a>
-                )}{" "}
-                {ctaText2}
-              </Button>
-            )}
+            <div className="mt-20">
+              <div className="mb-10">
+                <p className="h6 text-nextflow monospace mb-2">When</p>
+                <h3 className="h3">{when}</h3>
+              </div>
+              <div className="mb-10">
+                <p className="h6 text-nextflow monospace mb-2">Where</p>
+                <h3 className="h3">{where}</h3>
+              </div>
+              <div className="mb-10">
+                <p className="h6 text-nextflow monospace mb-2">Price</p>
+                <h3 className="h3">{price}</h3>
+              </div>
+            </div>
+            <h1 className="h0">2025</h1>
           </div>
         </div>
-        <div
-          className={clsx(
-            styles.rightWrapper,
-            "h-full flex flex-col justify-between md:text-right",
-          )}
-        >
-          <div className="mt-20">
-            <div className="mb-10">
-              <p className="h6 text-nextflow monospace mb-2">When</p>
-              <h3 className="h3">{when}</h3>
-            </div>
-            <div className="mb-10">
-              <p className="h6 text-nextflow monospace mb-2">Where</p>
-              <h3 className="h3">{where}</h3>
-            </div>
-            <div className="mb-10">
-              <p className="h6 text-nextflow monospace mb-2">Price</p>
-              <h3 className="h3">{price}</h3>
-            </div>
-          </div>
-          <h1 className="h0">2025</h1>
-        </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
