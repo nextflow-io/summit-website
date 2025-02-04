16760346b7394be824630f4bf8fa2a5d6eff6272
dana-seqera <dana.kim@seqera.io>
2025-02-03 20:08:29 -0500

    update contact

--- src/modules/Contact/index.tsx
+++ src/modules/Contact/index.tsx
@@ -24,13 +24,13 @@ const SectionContact: React.FC<Props> = ({ className }) => {
       <div className="flex -m-2 sm:-m-4 flex flex-wrap">
         <div className="w-full md:w-1/2 p-2 sm:p-4">
           <SectionBox href="mailto:help.summit@nextflow.io">
-            <h5 className="h5 mb-2">Ticketing questions</h5>
+            <p className="text-lg font-semibold font-display mb-2">Ticketing questions</p>
             <div className="monospace text-nextflow">help.summit@nextflow.io</div>
           </SectionBox>
         </div>
         <div className="w-full md:w-1/2 p-2 sm:p-4">
           <SectionBox href="mailto:summit@nextflow.io">
-            <h5 className="h5 mb-2">Program questions</h5>
+            <h5 className="text-lg font-semibold font-display mb-2">Program questions</h5>
             <div className="monospace text-nextflow">summit@nextflow.io</div>
           </SectionBox>
         </div>
