9b78c4263b6dec3924fb7c33c4b5afb34728479e
dana-seqera <dana.kim@seqera.io>
2025-02-08 15:06:08 -0500

    add faq and past events to register page

--- src/modules/PastEvents/index.tsx
+++ src/modules/PastEvents/index.tsx
@@ -1,9 +1,17 @@
-import Swiper from "@components/Swiper";
-import Arrow from "./Arrow";
-
-import events from "./events.mjs";
-
-import styles from "./styles.module.css";
+import Box, { SubSection } from "@components/Box";
+import img1 from "@images/photos/2025/past-events.jpg";
+import ArrowUpRight from "@icons/ArrowUpRight";
+import e2019_nf_camp from "@images/photos/past-events/e2019_nf_camp.jpg";
+import e2017_nf from "@images/photos/past-events/e2017_nf.jpg";
+import e2018_nf from "@images/photos/past-events/e2018_nf.jpg";
+import e2022_summit from "@images/photos/past-events/e2022_summit.jpg";
+import e2022_bcn from "@images/photos/past-events/e2022_bcn.jpg";
+import e2022_hack from "@images/photos/past-events/e2022_hack.jpg";
+import e2023_boston from "@images/photos/past-events/e2023_boston.jpg";
+import e2023_bcn from "@images/photos/past-events/e2023_bcn.jpg";
+import e2024_bcn from "@images/photos/past-events/e2024_bcn.jpg";
+import e2024_boston from "@images/photos/past-events/e2024_boston.jpg";
+import Button from "@components/Button";
 
 type Props = {
   title?: string;
@@ -11,31 +19,75 @@ type Props = {
 
 const SectionPastEvents: React.FC<Props> = ({ title }) => {
   return (
-    <section className="pb-16 container relative z-10">
-      <Swiper title={title}>
-        {events.map((event, index) => (
-          <a
-            key={index}
-            className={styles.card}
-            href={event.url}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={event.img.src} loading="lazy" />
-            <div className={styles.overlay} />
-            <div className={styles.overlay2} />
-            <div className={styles.content}>
-              <div>
-                <h4>{event.title}</h4>
-                <div className={styles.button}>
-                  Visit website
-                  <Arrow />
-                </div>
-              </div>
+    <section className="mt-20 mb-24 container">
+      <h1 className="h2 mb-10">See our past events</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        <Box
+          externalLink={true}
+          href="https://summit.nextflow.io/2024/barcelona/"
+          className="aspect-square"
+        >
+          <SubSection className="flex flex-col justify-between">
+            <div className="border-b border-b-nextflow w-full flex flex-row justify-between items-center pb-4">
+              <h5 className="h5">Barcelona 2024</h5>
+              <ArrowUpRight />
             </div>
-          </a>
-        ))}
-      </Swiper>
+            <div className="imageBlend mt-6 mb-8 relative h-0 w-full overflow-hidden pb-[80%]">
+              <img
+                className="max-w-none w-full h-full absolute top-0 bottom-0 left-0 right-0 object-cover"
+                src={e2024_bcn.src}
+                alt="Barcelona 2024 Nextflow Summit"
+              />
+            </div>
+          </SubSection>
+        </Box>
+
+        <Box
+          externalLink={true}
+          href="https://summit.nextflow.io/2024/boston/"
+          className="aspect-square"
+        >
+          <SubSection className="flex flex-col justify-between">
+            <div className="border-b border-b-nextflow w-full flex flex-row justify-between items-center pb-4">
+              <h5 className="h5">Boston 2024</h5>
+              <ArrowUpRight />
+            </div>
+            <div className="imageBlend mt-6 mb-8 relative h-0 w-full overflow-hidden pb-[80%]">
+              <img
+                className="max-w-none w-full h-full absolute top-0 bottom-0 left-0 right-0 object-cover"
+                src={e2024_boston.src}
+                alt="Boston 2024 Nextflow Summit"
+              />
+            </div>
+          </SubSection>
+        </Box>
+
+        <Box
+          externalLink={true}
+          href="https://summit.nextflow.io/2023/barcelona/"
+          className="aspect-square"
+        >
+          <SubSection className="flex flex-col justify-between">
+            <div className="border-b border-b-nextflow w-full flex flex-row justify-between items-center pb-4">
+              <h5 className="h5">Barcelona 2023</h5>
+              <ArrowUpRight />
+            </div>
+            <div className="imageBlend mt-6 mb-8 relative h-0 w-full overflow-hidden pb-[80%]">
+              <img
+                className="max-w-none w-full h-full absolute top-0 bottom-0 left-0 right-0 object-cover"
+                src={e2023_bcn.src}
+                alt="Barcelona 2023 Nextflow Summit presenters"
+              />
+            </div>
+          </SubSection>
+        </Box>
+      </div>
+      <div className="text-center">
+        <Button className="mt-10 relative" white arrowAfter>
+          <a className="absolute w-full h-full" href="/past-events"></a> See all
+          past events
+        </Button>
+      </div>
     </section>
   );
 };
--- src/pages/2025/boston/register/qa.ts
+++ src/pages/2025/boston/register/qa.ts
@@ -2,31 +2,42 @@ export default {
   main: [
     {
       question: "How do I register for the Nextflow Summit?",
-      answer: `Anyone is eligible to submit. THIS IS PLACEHOLDER COPY NEEDS TO BE UPDATED`,
+      answer:
+        `Find out more information on registering <a class="underline" href="/2025/boston/register">here</a>.`,
     },
     {
       question: "What is included in the registration fee?",
       answer:
-        "The deadline for this year's Boston Nextflow Summit is March 14, 2025.",
+        `Your ticket includes:
+          <ul>
+          <li>- Access to all in-person content and programming</li>
+          <li>- Enjoy meals and social events</li>
+          <li>- Network and connect with other attendees and sponsors</li>
+          </ul>
+          `
     },
     {
       question: "Are there early bird discounts?",
-      answer: "There are three themes this year",
+      answer:
+        `<ul><p>Enjoy our early bird discounts:</p>
+          <li>- Early bird 25% until February 28</li>
+          <li>- Early bird 15% until March 31</li></ul>
+          `
+    },
+    {
+      question: "Are there discounts for group registration?",
+      answer:
+        `Contact our team at <a href="mailto:help.summit@nextflow.io">help.summit@nextflow.io</a> to inquire about group registrations.`
     },
     {
       question: "What is the refund or cancellation policy for registration?",
-      answer: "THIS IS PLACEHOLDER COPY NEEDS TO BE UPDATED",
+      answer:
+        "We will guarantee a 100% refund if you cancel by April 25."
     },
     {
-      question:
-        "Can I transfer my registration to someone else if I’m unable to attend?",
-      answer:
-        "Abstracts will be read and presenters notified on a rolling basis, so apply soon. The final abstracts will be accepted and presenters notified by end of March.",
+        question: "Can I add training after I complete my registration?",
+        answer:
+          `Contact our team at <a href="mailto:help.summit@nextflow.io">help.summit@nextflow.io</a> to check if training seats are still available. If they are, we’ll assist you with registration.`
     },
-    {
-      question: "Will on-site registration be available during the event?",
-      answer:
-        "Abstracts will be read and presenters notified on a rolling basis, so apply soon. The final abstracts will be accepted and presenters notified by end of March.",
-    },
-  ],
-};
+  ]
+}
