6c54dd10a48b651b669731dbf56f630f0c6af7e7
dana-seqera <dana.kim@seqera.io>
2025-02-09 14:16:04 -0500

    call for abstracts add icons, updates

---
+++ src/images/icons/City.tsx
@@ -0,0 +1,36 @@
+import * as React from "react"
+const City = (props) => (
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={142}
+    height={142}
+    fill="none"
+    {...props}
+  >
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeMiterlimit={10}
+      strokeWidth={2}
+      d="M2.958 139.042V56.208l59.167-5.916v88.75M139.042 139.042V94.667l-29.584-5.917v50.292"
+    />
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeMiterlimit={10}
+      strokeWidth={2}
+      d="M50.291 51.475v-42.6l59.167-5.917v136.084M50.291 23.667l59.167-5.917"
+    />
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeMiterlimit={10}
+      strokeWidth={2}
+      d="M97.625 121.292h-17.75v17.75h17.75v-17.75ZM38.459 121.292h-17.75v17.75h17.75v-17.75ZM20.709 68.041h5.916M38.459 68.041h5.916M20.709 79.875h5.916M38.459 79.875h5.916M20.709 91.709h5.916M38.459 91.709h5.916M20.709 103.542h5.916M38.459 103.542h5.916M91.709 32.541h5.916M91.709 44.375h5.916M91.709 56.209h5.916M91.709 68.041h5.916M91.709 79.875h5.916M91.709 91.709h5.916M91.709 103.542h5.916M73.959 32.541h5.916M73.959 44.375h5.916M73.959 56.209h5.916M73.959 68.041h5.916M73.959 79.875h5.916M73.959 91.709h5.916M73.959 103.542h5.916M121.292 109.458h5.916M121.292 121.292h5.916M2.958 139.042h136.084"
+    />
+  </svg>
+)
+export default City
---
+++ src/images/icons/OpenSign.tsx
@@ -0,0 +1,36 @@
+import * as React from "react"
+const OpenSign = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={201}
+    height={145}
+    fill="none"
+    {...props}
+  >
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeMiterlimit={10}
+      strokeWidth={2}
+      d="M122.063 117.938h-17.251V74.812h17.251M104.812 100.688h12.938M70.313 117.938V74.812M139.312 117.938V74.812l17.251 43.126V74.812M44.438 74.813l-8.626 8.624v25.875l8.626 8.626 8.624-8.626V83.438l-8.624-8.626ZM70.313 74.813h17.25v17.25l-17.25 8.625"
+    />
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeMiterlimit={10}
+      strokeWidth={2}
+      d="M178.125 143.812H1.312l21.563-94.875h176.813l-21.563 94.875Z"
+    />
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeMiterlimit={10}
+      strokeWidth={2}
+      d="M163.031 48.938 111.281 1.5l-51.75 47.438"
+    />
+  </svg>
+)
+export default OpenSign
