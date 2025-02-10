e71b36a6bb9a3fad49fc714a1064ad2e5c79915c
dana-seqera <dana.kim@seqera.io>
2025-02-10 18:01:15 -0500

    nav updates

--- src/layouts/Header/SecondaryMenu.tsx
+++ src/layouts/Header/SecondaryMenu.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { motion } from "framer-motion";
 import menuLinks from "./menuLinks";
 import Link from "./Link";
 
@@ -9,36 +10,85 @@ type Props = {
   desktop?: boolean;
 };
 
-const Menu: React.FC<Props> = (props) => {
+const SecondaryMenu: React.FC<Props> = (props) => {
+  const [position, setPosition] = useState({
+    left: 0,
+    width: 0,
+    opacity: 0,
+  });
+
   const { pathname, namespace, desktop } = props;
 
   function isActive(path) {
-    return String(pathname) == String(path)
+    return String(pathname) == String(path);
   }
- 
-    
+
   let location;
   if (namespace === "2025/boston") location = 0;
   if (namespace === "2025/barcelona") location = 1;
 
   return (
     <nav>
-      <ul>
+      <ul
+        onMouseLeave={() => {
+          setPosition((pv) => ({
+            ...pv,
+            opacity: 0,
+          }));
+        }}
+        className="rounded-sm relative mx-auto flex w-fit bg-nextflow-200 overflow-hidden"
+      >
         {menuLinks[location].dropdowns?.map(({ name, url }, i) => {
           return (
-            <li key={i}>
-              {/* <Link href={url} active={isActive(url)}>
+            <div key={i}>
+              <Tab url={url} pathname={pathname} setPosition={setPosition}>
                 {name}
-              </Link> */}
-                <a href={url} className={`navItem ${pathname === url ? "is-active" : ""}`}>
-                {name}
-              </a>
-            </li>
+              </Tab>
+            </div>
           );
         })}
+        <Cursor position={position} />
       </ul>
     </nav>
   );
 };
 
-export default Menu;
+const Tab = ({ children, setPosition, url, pathname }) => {
+  const ref = useRef(null);
+
+  return (
+    <li
+      ref={ref}
+      onMouseEnter={() => {
+        if (!ref?.current) return;
+        const { width } = ref.current.getBoundingClientRect();
+        setPosition({
+          left: ref.current.offsetLeft,
+          width,
+          opacity: 1,
+        });
+      }}
+      className="relative z-10 block cursor-pointer text-brand"
+    >
+      <a
+        href={url}
+        className={`navItem ${pathname === url ? "is-active" : ""}`}
+      >
+        {children}
+      </a>
+    </li>
+  );
+};
+
+const Cursor = ({ position }) => {
+  return (
+    <motion.li
+      animate={{
+        ...position,
+      }}
+      className="nav__highlight"
+    />
+  );
+};
+
+export default SecondaryMenu;
