19c3af7c29d8e1647cdeb5d6650d64262efbe4bb
dana-seqera <dana.kim@seqera.io>
2025-05-06 10:36:26 -0500

    bcn updates

--- src/pages/2025/barcelona/overview/qa.ts
+++ src/pages/2025/barcelona/overview/qa.ts
@@ -5,36 +5,36 @@ export default {
       answer:
         `Registration will open up soon! Sign up for updates by subscribing <a href="#register">here</a>.`,
     },
-    {
-      question: "How much are the registration fees?",
-      answer:
-        `    <span class="underline">Training</span><br/>
-             Academic €120 <br/>
-             Corporate €300 
-          </p>
-          <p>
-          <span class="underline">Hackathon</span><br/>
-          Academic €120<br/>
-          Corporate $300</p> 
+  //   {
+  //     question: "How much are the registration fees?",
+  //     answer:
+  //       `    <span class="underline">Training</span><br/>
+  //            Academic €120 <br/>
+  //            Corporate €300 
+  //         </p>
+  //         <p>
+  //         <span class="underline">Hackathon</span><br/>
+  //         Academic €120<br/>
+  //         Corporate $300</p> 
 
-          <p>
-          <p>
-          <span class="underline">Summit</span><br/>
-            25% Early Bird, May 1 - June 30<br/>
-            Academic €150 <br/>
-            Corporate €300 <br/>
-          <br/>
+  //         <p>
+  //         <p>
+  //         <span class="underline">Summit</span><br/>
+  //           25% Early Bird, May 1 - June 30<br/>
+  //           Academic €150 <br/>
+  //           Corporate €300 <br/>
+  //         <br/>
 
-            15% Early Bird, July 1 - August 30<br/>
-            Academic €170 <br/>
-            Corporate €340 <br/><br/>
+  //           15% Early Bird, July 1 - August 30<br/>
+  //           Academic €170 <br/>
+  //           Corporate €340 <br/><br/>
 
-            Full Price, September 1 - October 13<br/>
-            Academic €200 <br/>
-            Corporate €400 <br/>
-          </p>
-          `
-  },
+  //           Full Price, September 1 - October 13<br/>
+  //           Academic €200 <br/>
+  //           Corporate €400 <br/>
+  //         </p>
+  //         `
+  // },
     {
       question: "What is included in the registration fee?",
       answer:
@@ -46,11 +46,11 @@ export default {
           </ul>
           `
     },
-    {
-      question: "When will the Call for Abstracts open?",
-      answer:
-        `The Call for Abstracts will open on May 1, 2025.`
-    },
+    // {
+    //   question: "When will the Call for Abstracts open?",
+    //   answer:
+    //     `The Call for Abstracts will open on May 1, 2025.`
+    // },
   ]
   
 }
