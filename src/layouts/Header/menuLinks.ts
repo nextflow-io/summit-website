4adf65fe425f5f1586ea0e645f20faa44e449a45
dana.kim@seqera.io <dana.kim@seqera.io>
2025-07-11 12:46:55 -0400

    update header

--- src/layouts/Header/menuLinks.ts
+++ src/layouts/Header/menuLinks.ts
@@ -1,5 +1,31 @@
 const menuLinks = [
   {
+    name: "Virtual",
+    url: "/2025/virtual",
+    // dropdowns: [
+    //   {
+    //     name: "Overview",
+    //     url: "/2025/barcelona/overview",
+    //   },
+    //   {
+    //     name: "Register",
+    //     url: "/2025/barcelona/register",
+    //   },
+    //   {
+    //     name: "Hackathon",
+    //     url: "/2025/barcelona/hackathon",
+    //   },
+    //   {
+    //     name: "Training",
+    //     url: "/2025/barcelona/training",
+    //   },
+    //   {
+    //     name: "Travel",
+    //     url: "/2025/barcelona/travel",
+    //   },
+    // ],
+  },
+  {
     name: "Barcelona",
     url: "/2025/barcelona",
     dropdowns: [
@@ -26,77 +52,51 @@ const menuLinks = [
     ],
   },
   {
-    name: "Virtual",
-    url: "/2025/virtual",
-    // dropdowns: [
-    //   {
-    //     name: "Overview",
-    //     url: "/2025/barcelona/overview",
-    //   },
-    //   {
-    //     name: "Register",
-    //     url: "/2025/barcelona/register",
-    //   },
-    //   {
-    //     name: "Hackathon",
-    //     url: "/2025/barcelona/hackathon",
-    //   },
-    //   {
-    //     name: "Training",
-    //     url: "/2025/barcelona/training",
-    //   },
-    //   {
-    //     name: "Travel",
-    //     url: "/2025/barcelona/travel",
-    //   },
-    // ],
+    name: "Boston",
+    url: "/2025/boston",
+    dropdowns: [
+      {
+        name: "Overview",
+        url: "/2025/boston/overview",
+      },
+      {
+        name: "Register",
+        url: "/2025/boston/register",
+      },
+      {
+        name: "Agenda",
+        url: "/2025/boston/agenda",
+      },
+      {
+        name: "Speakers",
+        url: "/2025/boston/speakers",
+      },
+      {
+        name: "Posters",
+        url: "/2025/boston/posters",
+      },
+      {
+        name: "Hackathon",
+        url: "/2025/boston/hackathon",
+      },
+      {
+        name: "Training",
+        url: "/2025/boston/training",
+      },
+      {
+        name: "Travel",
+        url: "/2025/boston/travel",
+      },
+      {
+        name: "Sponsors",
+        url: "/2025/boston/sponsors",
+      },
+      {
+        name: "Gallery",
+        url: "/2025/boston/gallery",
+      },
+    ],
   },
-  // {
-  //   name: "Boston",
-  //   url: "/2025/boston",
-  //   dropdowns: [
-  //     {
-  //       name: "Overview",
-  //       url: "/2025/boston/overview",
-  //     },
-  //     {
-  //       name: "Register",
-  //       url: "/2025/boston/register",
-  //     },
-  //     {
-  //       name: "Agenda",
-  //       url: "/2025/boston/agenda",
-  //     },
-  //     {
-  //       name: "Speakers",
-  //       url: "/2025/boston/speakers",
-  //     },
-  //     {
-  //       name: "Posters",
-  //       url: "/2025/boston/posters",
-  //     },
-  //     {
-  //       name: "Hackathon",
-  //       url: "/2025/boston/hackathon",
-  //     },
-  //     {
-  //       name: "Training",
-  //       url: "/2025/boston/training",
-  //     },
-  //     {
-  //       name: "Travel",
-  //       url: "/2025/boston/travel",
-  //     },
-  //     {
-  //       name: "Sponsors",
-  //       url: "/2025/boston/sponsors",
-  //     },
-  //     {
-  //       name: "Gallery",
-  //       url: "/2025/boston/gallery",
-  //     },
-  //   ],
-  // },
   {
     name: "Call for Abstracts",
     url: "/call-for-abstracts",
