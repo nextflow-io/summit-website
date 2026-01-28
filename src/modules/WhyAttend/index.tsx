765306c79bd4a89e75a9ebd913dd9cd40cac8de1
dana.kim@seqera.io <dana.kim@seqera.io>
2026-01-28 00:03:57 -0500

    why attend

--- src/modules/WhyAttend/index.tsx
+++ src/modules/WhyAttend/index.tsx
@@ -1,24 +1,52 @@
-import React from "react";
-import Box from "@components/Box";
-import img1 from "@images/photos/2026/barcelona/why-attend-bcn-2.jpg";
-import img2 from "@images/photos/2026/boston/why-attend.jpg";
-import img3 from "@images/photos/2026/barcelona/why-attend-bcn-3.jpg";
-import Training from "@icons/Training";
-import NextflowExperts from "@icons/NextflowExperts";
-import NextflowLearning from "@icons/NextflowLearning";
-import NextflowTeam from "@icons/NextflowTeam";
-import Lightbulb from "@icons/Lightbulb";
+import React from 'react';
+import LandingHero from '@components/LandingHero';
+import FeatureBlocks from '@modules/FeatureBlocks';
+import Faq from '@components/Faq';
 
 type Props = {
-  className?: string;
+  attend: any;
 };
 
-const WhyAttend: React.FC<Props> = ({ className }) => {
+const WhyAttendPage: React.FC<Props> = ({ attend }) => {
   return (
-    <div className={className}>
-      
-    </div>
+    <>
+      <LandingHero
+        title={attend.hero?.headline}
+        content={attend.hero?.bodycopy}
+        ctaText1={attend.hero?.button1?.buttonText}
+        ctaLink1={attend.hero?.button1?.buttonLink}
+        ctaText2={attend.hero?.button2?.buttonText}
+        ctaLink2={attend.hero?.button2?.buttonLink}
+        headlineSize={attend.hero?.headlineSize}
+      />
+
+      {attend.featureSection?.boxes &&
+        attend.featureSection.boxes.length > 0 && (
+          <FeatureBlocks
+            headline={attend.featureSection.headline}
+            boxes={attend.featureSection.boxes.map((box) => ({
+              title: box?.title?.title,
+              href: box?.title?.href?.url || box?.title?.href?.href,
+              externalLink: box?.title?.href?.external,
+              subtitleLeft: box?.subtitle?.subtitleLeft,
+              subtitleRight: box?.subtitle?.subtitleRight,
+              image: box?.image?.asset?.url,
+              imageAlt: box?.image?.alt,
+              bottomSubtitleLeft: box?.lowerSubtitle?.lowerSubtitleLeft,
+              bottomSubtitleRight: box?.lowerSubtitle?.lowerSubtitleRight,
+              headline: box?.headline,
+              bodycopy: box?.bodycopy,
+              buttonText: box?.cta?.buttonText,
+              buttonUrl: box?.cta?.buttonLink || box?.cta?.buttonUrl,
+            }))}
+          />
+        )}
+
+      {attend.faqSection && attend.faqSection.length > 0 && (
+        <Faq data={attend.faqSection} />
+      )}
+    </>
   );
 };
 
-export default WhyAttend;
+export default WhyAttendPage;
