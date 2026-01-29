ad4fe045387aaebd72fea7b714c100e66390363a
dana.kim@seqera.io <dana.kim@seqera.io>
2026-01-29 01:59:18 -0500

    Revert "clean up"
    This reverts commit 1043702ad030f0ed84a8626672cb432603f06d10.

---
+++ src/pages/2026/barcelona/overview/qa.ts
@@ -0,0 +1,69 @@
+export default {
+  main: [
+    {
+      question: "How do I register for the Nextflow Training and nf-core Hackathon Barcelona 2025?",
+      answer:
+      `<p>To register for Nextflow Training Barcelona 2025, click <a href="https://seqera.swoogo.com/nextflow-training" target="_blank">here</a>.</p>
+      <p>To register for the nf-core Hackathon Barcelona 2025, click <a href="https://seqera.swoogo.com/nfcore-hackathon" target="_blank">here</a>.</p>`,
+    },
+    {
+      question: "How much are the registration fees?",
+      answer:
+        `<p>Early-bird 25% discount available until August 15.</p>
+        
+        <p>   <span class="underline">Training (Barcelona)</span><br/>
+             Academic: €120  <br/>
+             Corporate: €300 
+          </p>
+          <p>
+          <span class="underline">Hackathon (Barcelona)</span><br/>
+          Academic: €120<br/>
+          Corporate: €120<br/><br/>
+            <i>Price includes 21% VAT</i><br /><br />
+          `
+  },
+  {
+    question: "Can I attend both Nextflow Training and nf-core Hackathon Barcelona 2025?",
+    answer:
+      `Nextflow Training and nf-core Hackathon run simultaneously over October 28-29, so you can only register to attend one.
+        `
+  },
+  {
+    question: "What is included in the registration fee for Training and Hackathon?",
+    answer:
+      `Your ticket includes:
+        <ul>
+        <li>Access to all in-person content and programming</li>
+        <li>Enjoy meals and social events</li>
+        <li>Network and connect with other attendees</li>
+        </ul>
+        `
+  },
+  {
+    question: "Are there early bird discounts?",
+    answer:
+      `<p>Early-bird 25% discount is available until August 15.</p>
+
+        `
+  },
+  {
+    question: "Are there discounts for group registration?",
+    answer:
+      `Contact our team at <a href="mailto:help.summit@nextflow.io">help.summit@nextflow.io</a> to inquire about group registrations.`
+  },
+  {
+    question: "Are there student discounts or financial assistance available?",
+    answer:
+      `<p>Accessibility is important to us. We strive to make the event affordable and available to the community. If you are interested in attending and registration cost is a barrier, please reach out to <a href="mailto:help.summit@nextflow.io">help.summit@nextflow.io</a> to discuss sliding scale discounts.</p>`
+},
+  {
+    question: "What is the refund or cancellation policy for registration?",
+    answer:
+      "We will guarantee a 100% refund if you cancel by October 17."
+  },
+  ]
+  
+}
+  
+  
+  
